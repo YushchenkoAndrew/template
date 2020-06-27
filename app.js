5ef743efb626f8fd1e2ce1932fabef0a0350e137
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-06-27 17:32:25 +0300

    Add a WebServer

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const HOST = "127.0.0.1";
+const PORT = 8080;
+
+// app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
+
+app.use(express.static("JS"));
+
+app.listen(PORT, HOST);
+console.log(`WebServer: http://${HOST}:${PORT}`);
