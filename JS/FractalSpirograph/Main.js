b5b14f500f5f60a01decdf0e8e3f7cd6c51ac2af
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-07-08 17:40:02 +0300

    Add mouse control

--- JS/FractalSpirograph/Main.js
+++ JS/FractalSpirograph/Main.js
@@ -1,11 +1,13 @@
 const W = 700;
 const H = 700;
 
-const R = 500;
-const CHANGE = 0.5;
+let R = 500;
+
 const SPEED = 0.1;
-const K = -2;
-const DIR = -1;
+
+let K = -2;
+let CHANGE = 0.5;
+let DIR = -1;
 
 let planet;
 let path = [];
@@ -16,10 +18,41 @@ function setup() {
   planet = new Planet(W / 2, H / 2);
 
   planet.createSatellite(2);
+
+  console.log("Menu for controlling Fractal Spirograph");
+  console.log("Figure will change on each 50 frame");
+  console.log("Mouse X - change amount of circles");
+  console.log("Mouse Y - change size of this circles");
+  console.log("On mouse Click - change equation on (-1)^n");
+  console.log("On double Click - change circles location");
+}
+
+function mousePressed() {
+  K *= -1;
+
+  createFigure();
+}
+
+function doubleClicked() {
+  DIR *= -1;
+  R = DIR == -1 ? 500 : 200;
+
+  createFigure();
+}
+
+function createFigure() {
+  CHANGE = mouseY / H;
+
+  planet = new Planet(W / 2, H / 2);
+  planet.createSatellite(Math.floor((mouseX / W) * 5) + 2);
 }
 
 function draw() {
   background(0);
+
+  if (!(frameCount % 50)) {
+    createFigure();
+  }
 
   for (let i = 0; i < 100; i++) {
     if (planet.isNewYear()) path = [];
