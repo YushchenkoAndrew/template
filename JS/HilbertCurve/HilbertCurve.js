e1cfec44fe4fb0cf7a4e94b71d2a569d7ed09eb4
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-07-19 20:46:00 +0300

    Improve visualization of Hilbert Curve

--- JS/HilbertCurve/HilbertCurve.js
+++ JS/HilbertCurve/HilbertCurve.js
@@ -1,19 +1,40 @@
+// Source:  https://en.wikipedia.org/wiki/Hilbert_curve
+// Source:  https://marcin-chwedczuk.github.io/iterative-algorithm-for-drawing-hilbert-curve
+
 class HilbertCurve {
   constructor(order = 1) {
     this.path = [];
-    this.order = order;
-    this.N = this.pow2(order);
-    let size = this.N * this.N;
-
-    console.log(size);
-
-    this.step = W / this.N;
-    this.offset = this.step / 2;
+    this.setOrder(order);
 
     // Initialize as a const Hilbert Curve First order
     this.FirstOrder = [createVector(0, 0), createVector(0, 1), createVector(1, 1), createVector(1, 0)];
 
-    for (let i = 0; i < size; i++) this.path.push(this.hilbertFunc(i));
+    this.index = 0;
+
+    // for (let i = 0; i < this.size; i++) this.path.push(this.hilbertFunc(i));
+  }
+
+  setOrder(order) {
+    this.order = order;
+    this.N = pow2(order);
+    this.size = this.N * this.N;
+
+    this.step = W / this.N;
+    this.offset = this.step / 2;
+
+    this.path = [...flipPath(this.path)];
+
+    function pow2(i) {
+      return 2 * (!(i - 1) ? 1 : pow2(i - 1));
+    }
+
+    function flipPath(path) {
+      let newPath = [];
+
+      for (let i in path) newPath.push(createVector(path[i].y, path[i].x));
+
+      return newPath;
+    }
   }
 
   hilbertFunc(i) {
@@ -22,15 +43,13 @@ class HilbertCurve {
     let offset = 1;
 
     for (let n = 1; n < this.order; n++) {
-      i = i >>> 2;
-      let x = pos.x;
-      let y = pos.y;
+      i = i >> 2;
 
       offset *= 2;
 
       switch (i & 3) {
         case 0: {
-          pos = createVector(y, x);
+          pos = createVector(pos.y, pos.x);
           break;
         }
 
@@ -38,42 +57,51 @@ class HilbertCurve {
           pos = p5.Vector.add(pos, createVector(0, offset));
           break;
         }
+
         case 2: {
           pos = p5.Vector.add(pos, createVector(offset, offset));
           break;
         }
+
         case 3: {
-          pos = createVector(offset - 1 - y, offset - 1 - x);
+          pos = createVector(offset - 1 - pos.y, offset - 1 - pos.x);
           pos = p5.Vector.add(pos, createVector(offset, 0));
           break;
         }
       }
-
-      //   i = i >>> 2;
     }
 
     return pos;
   }
 
-  pow2(i) {
-    return 2 * (!(i - 1) ? 1 : this.pow2(i - 1));
+  update() {
+    if (this.path.length == this.size) return true;
+
+    this.path.push(this.hilbertFunc(this.index++));
   }
 
-  show() {
-    stroke(255);
-    // strokeWeight(5);
+  show(showNum = 0) {
+    let coords = getCoords.bind(this);
+
     noFill();
 
-    beginShape();
+    for (let i = 1; i < this.path.length; i++) {
+      let color = ((i / this.size) * PI) / 2;
+      stroke(color, 255, 255);
 
-    for (let i in this.path) {
-      let x = this.path[i].x * this.step + this.offset;
-      let y = this.path[i].y * this.step + this.offset;
-      vertex(x, y);
+      line(...coords(i - 1), ...coords(i));
 
-      //   text(i, x + 5, y - 5);
+      if (showNum) {
+        let pos = coords(i - 1);
+        text(i, pos[0] + 5, pos[1] - 10);
+      }
     }
 
-    endShape();
+    function getCoords(i) {
+      let x = this.path[i].x * this.step + this.offset;
+      let y = this.path[i].y * this.step + this.offset;
+
+      return [x, y];
+    }
   }
 }
--- JS/HilbertCurve/Main.js
+++ JS/HilbertCurve/Main.js
@@ -1,17 +1,33 @@
 const W = 700;
-const H = 700;
+const H = W;
 
-const order = 8;
+let order = 1;
 let curve;
+
+// Custom variable
+let showNum = 0;
 
 function setup() {
   createCanvas(W, H);
 
+  // Change Color mode for Coloring in different colors
+  colorMode(HSB, 2 * PI, 255, 255);
+
   curve = new HilbertCurve(order);
+
+  console.log("Click Mouse for increase order for Hilbert Curve");
+  console.log("Set 'showNum = true' for showing number of each Node");
+
+  while (!curve.update());
+}
+
+function mouseClicked() {
+  curve.setOrder(++order);
 }
 
 function draw() {
   background(0);
 
-  curve.show();
+  curve.update();
+  curve.show(showNum);
 }
