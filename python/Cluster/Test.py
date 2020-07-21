9e3cc42f9bd3ebedb53f2410f6a7132b872ec3af
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-07-21 23:18:22 +0300

    Send and Recv on Cluster

--- python/Cluster/Test.py
+++ python/Cluster/Test.py
@@ -1,2 +1,23 @@
-for i in range(10):
-	print(f"raspi-master  --  {i}")
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+name = MPI.Get_processor_name()
+size = comm.Get_size()
+
+print('name = {0}   rank = {1}  size = {2}'.format(name, rank, size))
+
+#assert comm.size == 2
+
+if rank % 2 == 0:
+	data = {'a': 7, 'b': 3.14}
+	comm.send(data, dest=1, tag=11)
+	print('Send - 0')
+	data = comm.recv(source=1, tag=22)
+	print('data[0] = ', data)
+elif rank % 2 == 1:
+	data = comm.recv(source=0, tag=11)
+	print('data[1] = ', data)
+	data2 = 'Ok'
+	comm.send(data2, dest=0, tag=22)
+	print('Send - 1')
