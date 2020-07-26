e1ff103af4fc97476a1506095e1bc06f45816f10
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-07-26 21:44:55 +0300

    Complite Reaction Diffusion Alg on C++

--- C++/ReactionDiffusionAlg/Diffusion.h
+++ C++/ReactionDiffusionAlg/Diffusion.h
@@ -1,5 +1,8 @@
 #include <string.h>
 #include <fstream>
+#include "lib/bitmap_image.hpp"
+#include <cmath>
+#include <string>
 
 namespace ReactionDiffusionAlg
 {
@@ -12,31 +15,31 @@ namespace ReactionDiffusionAlg
         double b;
     };
 
-    typedef struct tagBITMAPFILEHEADER
+    struct BITMAPFILEHEADER
     {
-        unsigned short bfType;
-        uint32_t bfSize;
-        unsigned short bfReserved1;
-        unsigned short bfReserved2;
-        uint32_t bfOffBits;
-    } BITMAPFILEHEADER, *PBITMAPFILEHEADER;
+        unsigned short bfType{0x4D42};
+        unsigned long bfSize{0};
+        unsigned short bfReserved1{0};
+        unsigned short bfReserved2{0};
+        unsigned long bfOffBits{0};
+    };
 
-    typedef struct tagBITMAPINFOHEADER
+    struct BITMAPINFOHEADER
     {
-        uint32_t biSize;
-        int biWidth;
-        int biHeight;
-        unsigned short biPlane;
-        unsigned short biBitCount;
-        uint32_t biCompression;
-        uint32_t biSizeImage;
-        int biXPelsPerMeter;
-        int biYPelsPerMeter;
-        uint32_t biClrUsed;
-        uint32_t biClrImportant;
-    } BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+        unsigned long biSize{0};
+        unsigned long biWidth{0};
+        unsigned long biHeight{0};
+        unsigned short biPlane{1};
+        unsigned short biBitCount{0};
+        unsigned long biCompression{0};
+        unsigned long biSizeImage{0};
+        unsigned long biXPelsPerMeter{0};
+        unsigned long biYPelsPerMeter{0};
+        unsigned long biClrUsed{0};
+        unsigned long biClrImportant{0};
+    };
 
-    typedef struct tagRGBQUAD
+    typedef struct RGBQUAD
     {
         unsigned char rgbBlue;
         unsigned char rgbGreen;
@@ -51,11 +54,12 @@ namespace ReactionDiffusionAlg
         enum
         {
             W = 500,
-            H = 500
+            H = 500,
         };
 
-        Diffusion(double feed = 0.055, double kill = 0.062) : feed_(feed), kill_(kill), diffusion_(Chemical(1, 1))
+        Diffusion(double feed = 0.0367, double kill = 0.0649) : feed_(feed), kill_(kill), diffusion_(Chemical(1, 0.5))
         {
+            addChemical(W / 2, H / 2);
         }
 
         void addChemical(int x, int y, int range = 10)
@@ -86,7 +90,8 @@ namespace ReactionDiffusionAlg
                 }
             }
 
-            swap(curr, next);
+            swap();
+            epoch++;
         }
 
         Chemical LaplacianFunc(int x, int y)
@@ -105,63 +110,83 @@ namespace ReactionDiffusionAlg
             return result;
         }
 
-        void swap(Chemical a[H][W], Chemical b[H][W])
+        void swap()
         {
             for (int i = 0; i < H; i++)
             {
                 for (int j = 0; j < W; j++)
                 {
-                    Chemical temp = b[i][j];
-                    b[i][j] = a[i][j];
-                    a[i][j] = temp;
+                    Chemical temp = curr[i][j];
+                    curr[i][j] = next[i][j];
+                    next[i][j] = temp;
                 }
             }
         }
+
+        // void save()
+        // {
+        //     std::ofstream img{"Diffusion.bmp", std::ios_base::binary};
+
+        //     BITMAPFILEHEADER bmfh;
+        //     BITMAPINFOHEADER bmih;
+        //     RGBQUAD color = {0, 0, 255, 255};
+
+        //     memset(&color, 0, sizeof(RGBQUAD));
+        //     memset(&bmfh, 0, sizeof(bmfh));
+
+        //     int width = 35;
+        //     int height = 50;
+
+        //     bmfh.bfType = 0x4D42;                                         // declare to 'BM'
+        //     bmfh.bfOffBits = sizeof(bmfh) + sizeof(bmih) + sizeof(color); // specifies the offset from the beginning of the file to the bitmap data
+        //     bmfh.bfSize = bmfh.bfOffBits + sizeof(unsigned short) * width * height + height * ((sizeof(unsigned short) * width) % 4);
+
+        //     memset(&bmih, 0, sizeof(bmih));
+        //     bmih.biSize = sizeof(bmih);
+        //     bmih.biBitCount = 1;
+        //     bmih.biClrUsed = 32768;
+        //     bmih.biCompression = 0;
+        //     bmih.biHeight = height;
+        //     bmih.biWidth = width;
+        //     bmih.biPlane = 1;
+
+        //     img.write((const char *)(&bmfh), sizeof(bmfh));
+        //     img.write((const char *)(&bmih), sizeof(bmih));
+
+        //     img.write((const char *)(&color), sizeof(color));
+
+        //     for (int row = 0; row < height; row++)
+        //     {
+        //         for (int col = 0; col < width; col++)
+        //         {
+        //             img.write((char *)(&color), sizeof(color));
+        //         }
+        //         img.write((char *)(&color), (sizeof(color) * 35) % 4);
+        //     }
+        // }
 
         void save()
         {
-            std::ofstream img;
-            img.open("Diffusion.bmp");
+            bitmap_image image(W, H);
+            image.clear();
 
-            BITMAPFILEHEADER bmfh;
-            BITMAPINFOHEADER bmih;
-            unsigned char Palette[1024];
-
-            unsigned char color = 100;
-
-            memset(Palette, 0, 1024);
-            memset(&bmfh, 0, sizeof(bmfh));
-
-            int width = 35;
-            int height = 50;
-
-            bmfh.bfType = 0x4D42;                                // declare to 'BM'
-            bmfh.bfOffBits = sizeof(bmfh) + sizeof(bmih) + 1024; // specifies the offset from the beginning of the file to the bitmap data
-            bmfh.bfSize = bmfh.bfOffBits + sizeof(unsigned short) * width * height + height * ((sizeof(unsigned short) * width) % 4);
-
-            memset(&bmih, 0, sizeof(bmih));
-            bmih.biSize = sizeof(bmih);
-            bmih.biBitCount = 1;
-            bmih.biClrUsed = 32768;
-            bmih.biCompression = 0;
-            bmih.biHeight = height;
-            bmih.biWidth = width;
-            bmih.biPlane = 1;
-
-            img.write((char *)(&bmfh), sizeof(bmfh));
-            img.write((char *)(&bmih), sizeof(bmih));
-
-            img.write((char *)(Palette), 1024);
-
-            for (int row = 0; row < height; row++)
+            for (unsigned int i = 0; i < image.height(); i++)
             {
-                for (int col = 0; col < width; col++)
+                for (unsigned int j = 0; j < image.width(); j++)
                 {
-                    img.write((char *)(&color), sizeof(color));
+                    double a = curr[i][j].a;
+                    double b = curr[i][j].b;
+                    double sub = a - b;
+
+                    unsigned char color = floor(sub < 0 ? 0 : sub * 255);
+                    image.set_pixel(j, i, color, color, color);
                 }
-                img.write((char *)(Palette), (sizeof(color) * 35) % 4);
             }
+
+            image.save_image("Diffusion" + std::to_string(epoch) + ".bmp");
         }
+
+        int epoch = 0;
 
     private:
         Chemical curr[H][W];
--- C++/ReactionDiffusionAlg/Main.cpp
+++ C++/ReactionDiffusionAlg/Main.cpp
@@ -5,9 +5,19 @@ using namespace ReactionDiffusionAlg;
 
 int main()
 {
-    Diffusion diff = Diffusion();
-    diff.update();
-    diff.save();
+    Diffusion diff = Diffusion(0.0367, 0.0649);
+
+    while (diff.epoch < 100000)
+    {
+        diff.update();
+        if (diff.epoch % 50 == 0)
+        {
+            std::cout << "Yep \nEpoch = " << diff.epoch << "\n";
+            diff.save();
+        }
+    }
+
+    std::cout << "Done! \n";
 
     return 0;
 }
