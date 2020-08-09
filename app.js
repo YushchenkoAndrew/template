25187a37d25c04fd8f9328c6f8df902b6b225211
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-08-09 18:46:43 +0300

    Impove Back-End -- print self status

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const app = express();
+const time = new Date();
 
 const HOST = "0.0.0.0";
 const PORT = 8000;
 
-// app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
-// app.get("/projects", (req, res) => app.use(express.static("JS")));
+app.get("/projects/*", (req, res, next) => {
+  console.log(`~ Get request to ${req.url}`);
+  console.log(`\t=> At ${time}`);
+  console.log(`\t=> From Client ${req.connection.remoteAddress}\n`);
+  next();
+});
 
 app.use("/projects", express.static("JS"));
 
+app.get("/*", (req, res) => {
+  console.log(`\n~ Unexpected URL: ${req.url}`);
+  console.log(`\t=> File '${req.url.split("/").pop()}' not found\n`);
+  res.status(404);
+  res.send("404: File Not Found");
+});
+
 process.on("SIGINT", function () {
+  console.log("~ Server Terminated");
   process.exit();
 });
 
-app.listen(PORT, HOST);
-console.log(`WebServer: http://${HOST}:${PORT}`);
+app.listen(PORT, HOST, (err) => {
+  if (err) console.log(`~ Error appear ${err}`);
+  console.log("~ Server Started ...");
+  console.log(`~ Listening on Port ${PORT}`);
+});
