150ce654f9e2df2591c012ab02faf13284ff6b30
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-08-19 18:29:40 +0300

    Add Menu

--- JS/FlowField/Main.js
+++ JS/FlowField/Main.js
@@ -1,5 +1,6 @@
 const W = Math.floor(window.innerWidth / 10) * 10;
 const H = Math.floor(window.innerHeight / 10) * 10;
+const MENU_SIZE = 24;
 
 const step = 10;
 
@@ -8,6 +9,8 @@ const VelLimit = 5.2;
 
 // Custom variable
 let showFlow = false;
+
+let menu;
 
 let flow;
 
@@ -18,6 +21,14 @@ function setup() {
   console.log(`W = ${W} --- H = ${H}`);
 
   flow = new FlowField(2000);
+  menu = new Menu("Show Flow");
+  menu.completeAll();
+  menu.setOptions(() => {
+    showFlow = menu.items[0].enable;
+    if (!showFlow) background(255);
+  });
+
+  menu.items[0].enable = false;
 
   console.log("Set 'showFlow = true' for showing Vectors");
 
@@ -25,11 +36,14 @@ function setup() {
 }
 
 function mouseClicked() {
+  if (menu.IsEnable() && !menu.enableOption(mouseX, mouseY, true)) return;
   background(255, 150);
 }
 
 function draw() {
   if (showFlow) background(255);
+
+  menu.show();
 
   flow.calcFlow();
   flow.show(showFlow);
