eb7015d628152891b5525e23d17559fa858f5e48
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-08-24 03:32:42 +0300

    Add some changes

--- JS/app.js
+++ JS/app.js
@@ -11,3 +11,14 @@ function scrollFunc() {
   if (window.pageYOffset >= sticky) menu.classList.add("sticky");
   else menu.classList.remove("sticky");
 }
+
+$.get("https://www.cloudflare.com/cdn-cgi/trace", (data) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/guest", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(
+    JSON.stringify({
+      data: data,
+    })
+  );
+});
