d94d5f291391239c2db6c61c7837f6ce2248d9f0
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-09-03 00:27:26 +0300

    Add Controller fuctional (Sequelize)

--- JS/MySQL/controllers/Controller.js
+++ JS/MySQL/controllers/Controller.js
@@ -1,5 +1,43 @@
-const db = require("../models/index.js");
+const { Visitors } = require("../models/index.js");
 
-exports.print = () => db.Visitors.findAll().then((data) => console.log(data[0].dataValues));
+exports.print = () => Visitors.findAll();
 
-exports.findAll = (condition) => {};
+exports.findAll = (key, value) => {
+  let condition = {};
+  condition[key] = value || null;
+
+  return Visitors.findAll({ where: condition });
+};
+
+exports.create = (data) => {
+  let newVisitor = {};
+
+  for (let i in data) {
+    let params = data[i].split("=");
+    newVisitor[params[0]] = params[1] || null;
+  }
+
+  return Visitors.create(newVisitor);
+};
+
+exports.delete = (key, value) => {
+  let condition = {};
+  condition[key] = value;
+
+  return Visitors.destroy({ where: condition });
+};
+
+exports.update = (data) => {
+  let condition = data.splice(0, 1)[0].split("=");
+  let where = {};
+  where[condition[0]] = condition[1];
+
+  let newValue = {};
+
+  for (let i in data) {
+    let params = data[i].split("=");
+    newValue[params[0]] = params[1];
+  }
+
+  return Visitors.update(newValue, { where: where });
+};
