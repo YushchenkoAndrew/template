21faf9099378793685bb19492694e529b09012e9
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-09-02 16:25:47 +0300

    Clean some code

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,3 @@
-window.onscroll = function () {
-  scrollFunc();
-};
-
-var menu = document.getElementById("menu");
-var sticky = menu ? menu.offsetTop : undefined;
-
-function scrollFunc() {
-  if (!sticky) return;
-
-  if (window.pageYOffset >= sticky) menu.classList.add("sticky");
-  else menu.classList.remove("sticky");
-}
-
 $.get("https://www.cloudflare.com/cdn-cgi/trace", (data) => {
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "/guest", true);
