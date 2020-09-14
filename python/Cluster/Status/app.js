d0c6fca51b5fc2a6380bf45a88c6bb2647112b1f
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-09-14 21:53:40 +0300

    Change default input

--- python/Cluster/Status/app.js
+++ python/Cluster/Status/app.js
@@ -6,29 +6,27 @@ const HOST = "0.0.0.0";
 const PORT = 18000;
 
 // Default setup
-var pythonSript = spawn("python", ["./Main.py", "g", 0]);
+var pythonScript = spawn("python", ["./Main.py", "rainbow", 5]);
 
 app.get("/led/:command/:value", (req, res) => {
   console.log("~ Recieve request to change LED");
 
   // Send SIGTERM to process
-  pythonSript.kill("SIGKILL")
+  pythonScript.kill("SIGKILL");
 
   // Run python script
-  pythonSript = spawn("python", ["./Main.py", req.params.command, req.params.value]);
+  pythonScript = spawn("python", ["./Main.py", req.params.command, req.params.value]);
 
-  pythonSript.stdout.on("data", (data) => {
+  pythonScript.stdout.on("data", (data) => {
     console.log("~ Returned Data form python script");
     console.log(`\t=> ${data.toString().split("\n").join("\n\t=> ")}`);
   });
 
-  pythonSript.stderr.on('data', (data) => {
+  pythonScript.stderr.on("data", (data) => {
     console.log(data.toString());
-  })
-
+  });
 
   res.send("OK");
-
 });
 
 process.on("SIGINT", function () {
