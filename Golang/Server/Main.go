6a050a4c5bc19a03aa01d394daca84ed299b6241
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-09-30 15:02:06 +0300

    Add some changes

--- Golang/Server/Main.go
+++ Golang/Server/Main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world, Path = %s", r.URL.Path)
+	// fmt.Fprintf(w, "Request = %s", r.Method)
+
+	switch r.Method {
+	case "GET":
+		log.Println("server: Receive GET Request")
+	case "POST":
+		log.Println("server: Receive POST Request")
+	}
+
 	log.Printf("server: Receive request from %s", r.URL.Path)
 }
 
@@ -16,10 +24,10 @@ func main() {
 	var HOST string = "0.0.0.0"
 	var PORT int = 1337
 
-	fmt.Println("~ Server Started ...")
-	fmt.Printf("~ Listening on Port %d\n", PORT)
+	log.Println("[Info] Server Started ...")
+	log.Printf("[Info] Listening on Port %d\n", PORT)
 
 	http.HandleFunc("/test", requestHandler)
-	http.ListenAndServe(HOST+":"+strconv.Itoa(PORT), nil)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", HOST, PORT), nil)
 
 }
--- Golang/Testing/Main.go
+++ Golang/Testing/Main.go
@@ -17,17 +17,34 @@ func main() {
 	var a string = "Test1"
 	var b string = "Test2"
 
+	var arr [10]int
+
 	fmt.Println(swap(a, b))
 
 	for i := 0; i < 10; i++ {
+		arr[i] = i * i
 		fmt.Println(test(i))
 	}
+
+	fmt.Println(arr)
 
 	fmt.Println("While loop:")
 
 	// While loop
-	index := 0
+	var index = 0
 	for ; index < 10; index++ {
 		fmt.Println(test(index))
 	}
+
+	// Range
+	for k, v := range arr {
+		fmt.Printf("key = %d, value = %d\n", k, v)
+	}
+
+	// Slice
+	var s = arr[2:6]
+	s = append(s, 52)
+
+	fmt.Println(s)
+
 }
