b95dae90ff9119df94a0441cfade816138f8b6ed
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-01-10 13:46:47 +0200

    Simplify code

--- JS/ASCII-art/index.js
+++ JS/ASCII-art/index.js
@@ -1,62 +1,34 @@
 const pixel = require("get-pixels");
 const { writeFileSync } = require("fs");
-const { resolve } = require("path");
 
 function getPixels(path) {
   return new Promise((resolve, reject) => pixel(path, (err, data) => (err ? reject(err) : resolve(data))));
 }
 
-function grayScaleImage(pixels) {
-  if (!Array.isArray(pixels)) throw Error("Not an Array");
+function convertToAscii(path, { scale = 1, repeat = 2 } = {}) {
+  return new Promise(async (resolve, reject) => {
+    let {
+      shape: [W, H, pxD],
+      data,
+    } = await getPixels(path);
 
-  let grayPixels = [];
-  for (let i = 0; i < pixels.length; i += 4) {
-    grayPixels.push(Math.floor(pixels.slice(i, i + 3).reduce((acc, curr) => acc + curr) / 3));
-  }
+    let str = "";
+    let ascii = ' `^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$';
 
-  return grayPixels;
-}
-
-function get2D(pixels, [W, H], { scale = 1 }) {
-  if (!Array.isArray(pixels)) throw Error("Not an Array");
-
-  let data = [];
-  let index = 0;
-  for (let i = 0; i < H; i += scale) {
-    data.push([]);
-
-    for (let j = 0; j < W; j += scale) {
-      data[index].push(pixels[j + i * W]);
+    for (let i = 0; i < H; i += scale) {
+      for (let j = 0; j < W; j += scale) {
+        let index = (j + i * W) * pxD;
+        let gray = Math.floor(data.slice(index, index + 3).reduce((acc, curr) => acc + curr) / 3);
+        str += ascii.charAt(Math.floor((1 - gray / 255) * (ascii.length - 1))).repeat(repeat);
+      }
+      str += "\n";
     }
 
-    index++;
-  }
-
-  return data;
+    resolve(str);
+  });
 }
 
-function printAscii(pixels) {
-  if (!Array.isArray(pixels)) throw Error("Not an Array");
-
-  let ascii = ' `^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$';
-  let str = "";
-
-  for (let i in pixels) {
-    if (!Array.isArray(pixels[i])) return reject("Not an Array");
-
-    for (let j in pixels) str += ascii.charAt(Math.floor((1 - pixels[i][j] / 255) * (ascii.length - 1))).repeat(2);
-    str += "\n";
-  }
-
-  console.log(str);
-  return str;
-}
-
-async function convertToAscii(path, options = {}) {
-  let { shape, data } = await getPixels(path);
-  let pixelsMap = get2D(grayScaleImage(Array.from(data)), shape, options);
-
-  return new Promise((resolve, reject) => resolve(printAscii(pixelsMap)));
-}
-
-convertToAscii("./Shark.png", { scale: 5 }).then((data) => writeFileSync("ACII-art.txt", data));
+convertToAscii("./Shark.png", { scale: 7, repeat: 2 }).then((data) => {
+  console.log(data);
+  writeFileSync("ASCII-art.txt", data);
+});
