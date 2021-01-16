90fad69368df13d824eaf12f34260340e30ad6d5
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-01-17 01:22:46 +0200

    Create program for training neural network

---
+++ JS/HandwrittenDigit/Training/index.js
@@ -0,0 +1,40 @@
+let numberClassifier;
+let data = [];
+
+function preload() {
+  for (let i = 0; i < 10; i++) {
+    data.push([]);
+    for (let j = 0; j < 1000; j++) {
+      let index = `000${j + 1}`.slice(-4);
+      data[i][j] = loadImage(`data/Handwriting-${i}-${index}.png`);
+    }
+  }
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+
+  let options = {
+    inputs: [28, 28, 4],
+    task: "imageClassification",
+    debug: true,
+  };
+
+  numberClassifier = ml5.neuralNetwork(options);
+
+  for (let i in data) {
+    for (let j in data[i]) numberClassifier.addData({ image: data[i][j] }, { label: i });
+  }
+
+  numberClassifier.normalizeData();
+
+  numberClassifier.train({ epochs: 100 }, finishTraining);
+}
+
+function finishTraining() {
+  console.log("Training Finished");
+  numberClassifier.save();
+}
+
+function draw() {}
