41d918a49c488e8d7bc19cf2506f0284cf62853f
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-01-24 17:45:40 +0200

    Add callback function for movement

--- python/Snake/v3/Main.py
+++ python/Snake/v3/Main.py
@@ -1,8 +1,26 @@
 from SnakeGame import *
 
+import pygame
+
+
+def moveByPlayer(events):
+  DIRECTION = { pygame.K_UP: "up", pygame.K_DOWN: "down", pygame.K_LEFT: "left", pygame.K_RIGHT: "right" }
+  direction = None
+
+  for event in events:
+    if not direction:
+      direction = DIRECTION.get(event.key if event.type == pygame.KEYDOWN else None, None)
+
+  return direction
+
 def main():
-  game = SnakeGame((10, 50, 500, 500), (700, 700), 20)
-  game.draw()
+  screen = pygame.display.set_mode((700, 700))
+  game = SnakeGame((10, 50, 500, 500), screen, 20)
+  game.draw(moveByPlayer)
+
 
 if __name__ == "__main__":
+  # Initialize pygame
+  pygame.init()
   main()
+  pygame.quit()
--- python/Snake/v3/SnakeGame.py
+++ python/Snake/v3/SnakeGame.py
@@ -5,74 +5,81 @@ import pygame
 
 class SnakeGame:
   canvas = (0, 0, 500, 500)
-  window = (1000, 500)
   grid = None
   step = 10
 
   SPEED = 10
   run = True
 
-  def __init__(self, canvas, window, step = 20, showGrid = True):
-    # Initialize pygame
-    pygame.init()
-
-    self.screen = pygame.display.set_mode(window)
+  def __init__(self, canvas, screen, step = 20, showGrid = True):
     self.clock = pygame.time.Clock()
 
     self.canvas = canvas
-    self.window = window
+    self.screen = screen
     self.showGrid = showGrid
     self.step = step
     self.grid = [i // step for i in canvas[2:]]
 
-    print("Initialize grid:", self.grid)
+    self.printMessage('Initialize grid:', self.grid)
 
-    self.snake = Snake([i // 2 for i in self.grid], step)
+    self.snake = Snake([i // 2 for i in self.grid], step, self.grid)
     self.apple = Apple(self.grid, step)
 
 
+  def printMessage(self, *message):
+    print('\033[1;33;40mGame:\033[0m', *message)
+
+
   def listenToEvents(self, events):
-    direction = { pygame.K_UP: "up", pygame.K_DOWN: "down", pygame.K_LEFT: "left", pygame.K_RIGHT: "right" }
     step = None
 
     for event in events:
       if (event.type == pygame.QUIT):
         self.run = False
 
-      if not (step):
-        step = direction.get(event.key if event.type == pygame.KEYDOWN else None, None)
 
-    self.snake.move(step, self.apple.getPos(), self.apple.setPos)
-
+  def moveSnake(self, direction):
+    self.snake.move(direction, self.apple.pos, self.apple.setPos)
 
 
   def drawGrid(self):
     color = (150,) * 3
     [x, y, w, h] = self.canvas
 
+    if self.showGrid:
+      for i in range(self.grid[1]):
+        dy = self.step * i
 
-    for i in range(self.grid[1]):
-      dy = self.step * i
+        for j in range(1, self.grid[0]):
+          dx = self.step * j
 
-      for j in range(1, self.grid[0]):
-        dx = self.step * j
-
-        pygame.draw.line(self.screen, color, (x + dx, y), (x + dx, y + h - 1))
-        pygame.draw.line(self.screen, color, (x, y + dy), (x + w - 1, y + dy))
+          pygame.draw.line(self.screen, color, (x + dx, y), (x + dx, y + h - 1))
+          pygame.draw.line(self.screen, color, (x, y + dy), (x + w - 1, y + dy))
 
     pygame.draw.rect(self.screen, (255,) * 3, (x, y, w, h), 1)
 
-  def draw(self):
+
+  def draw(self, getDirection):
     while self.run:
-      self.screen.fill((0,) * 3)
+      # Fill canvas with black
+      pygame.draw.rect(self.screen, (0,) * 3, self.canvas)
       self.clock.tick(self.SPEED)
-      self.listenToEvents(pygame.event.get())
+
+      # Event Handler
+      events = pygame.event.get()
+      self.listenToEvents(events)
+      self.moveSnake(getDirection(events))
 
       # Draw
       self.drawGrid()
       self.snake.draw(self.screen, self.canvas[:2])
       self.apple.draw(self.screen, self.canvas[:2])
 
+      # Check if it a game over
+      if not self.snake.alive:
+        break
+
       # Flip the display
       pygame.display.flip()
-    pygame.quit()
+
+    self.printMessage('Game over')
