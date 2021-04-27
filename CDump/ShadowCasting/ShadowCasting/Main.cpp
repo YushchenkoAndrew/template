3f413f83e1b40b5a614fbd9d5a9f792c5f976b78
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-04-27 18:58:49 +0300

    Start working on Shadow Casting Alg, find Polygon Edges

--- CDump/ShadowCasting/ShadowCasting/Main.cpp
+++ CDump/ShadowCasting/ShadowCasting/Main.cpp
@@ -1,8 +1,29 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+#include <stdio.h>
+
+#define NORTH 0
+#define SOUTH 1
+#define WEST  2
+#define EAST  3
+
+#define INDEX(X, Y, STEP) (((Y) * (STEP)) + (X))
+#define NORTH_NEIGBOR(X, Y, STEP) (INDEX(X, Y - 1, STEP))
+#define SOUTH_NEIGBOR(X, Y, STEP) (INDEX(X, Y + 1, STEP))
+#define WEST_NEIGBOR(X, Y, STEP) (INDEX((X - 1), Y, STEP))
+#define EAST_NEIGBOR(X, Y, STEP) (INDEX((X + 1), Y, STEP))
+
+
+struct sEdge {
+	int sx, sy;
+	int ex, ey;
+};
+
 struct sCell {
 	bool exist = false;
+	bool edge_exist[4];
+	int edge_id[4];
 };
 
 class ShadowCasting : public olc::PixelGameEngine {
@@ -21,14 +42,131 @@ public:
 	bool OnUserUpdate(float fElapsedTime) override {
 		olc::vi2d mouse = GetMousePos();
 		
-		if (GetMouse(0).bPressed) {
-			int index = (mouse.y / iBlockSize) * iWorldWidth + mouse.x / iBlockSize;
-			
+		if (GetMouse(0).bReleased) {
+			int index = INDEX(mouse.x / iBlockSize, mouse.y / iBlockSize, iWorldWidth);
 			world[index].exist = !world[index].exist;
+
+			ConvertBitMapIntoPolyMap(0, 0, iWorldWidth, iWorldHeigh, iBlockSize, iWorldWidth);
 		}
 
 		Draw();
 		return true;
+	}
+
+	void ConvertBitMapIntoPolyMap(int sx, int sy, int w, int h, int iBlockSize, int pitch) {
+
+		// Reset PolyMap
+		vEdge.clear();
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				int index = INDEX(x, y, pitch);
+
+				for (int j = 0; j < 4; j++) {
+					world[index].edge_exist[j] = 0;
+					world[index].edge_id[j] = 0;
+				}
+			}
+		}
+
+		for (int x = 1; x < w - 1; x++) {
+			for (int y = 1; y < h - 1; y++) {
+
+				// Calculate index and all neighbors
+				int i = INDEX(x + sx, y + sy, pitch);
+				int n = NORTH_NEIGBOR(x + sx, y + sy, pitch);
+				int s = SOUTH_NEIGBOR(x + sx, y + sy, pitch);
+				int w = WEST_NEIGBOR(x + sx, y + sy, pitch);
+				int e = EAST_NEIGBOR(x + sx, y + sy, pitch);
+
+				if (world[i].exist) {
+
+					// Check WEST SIDE
+					if (!world[w].exist) {
+						if (world[n].edge_exist[WEST]) {
+							// Increase bounder of the edge
+							vEdge[world[n].edge_id[WEST]].ey += iBlockSize;
+							world[i].edge_id[WEST] = world[n].edge_id[WEST];
+							world[i].edge_exist[WEST] = true;
+						}
+						else {
+							sEdge edge;
+							edge.sx = x * iBlockSize; edge.sy = y * iBlockSize;
+							edge.ex = x * iBlockSize; edge.ey = (y + 1) * iBlockSize;
+
+							int edge_id = vEdge.size();
+							vEdge.push_back(edge);
+
+							world[i].edge_exist[WEST] = true;
+							world[i].edge_id[WEST] = edge_id;
+						}
+					}
+
+					// Check EAST SIDE
+					if (!world[e].exist) {
+						if (world[n].edge_exist[EAST]) {
+							// Increase bounder of the edge
+							vEdge[world[n].edge_id[EAST]].ey += iBlockSize;
+							world[i].edge_id[EAST] = world[n].edge_id[EAST];
+							world[i].edge_exist[EAST] = true;
+						}
+						else {
+							sEdge edge;
+							edge.sx = (x + 1) * iBlockSize; edge.sy = y * iBlockSize;
+							edge.ex = (x + 1) * iBlockSize; edge.ey = (y + 1) * iBlockSize;
+
+							int edge_id = vEdge.size();
+							vEdge.push_back(edge);
+
+							world[i].edge_exist[EAST] = true;
+							world[i].edge_id[EAST] = edge_id;
+						}
+					}
+
+					// Check NORTH SIDE
+					if (!world[n].exist) {
+						if (world[w].edge_exist[NORTH]) {
+							// Increase bounder of the edge
+							vEdge[world[w].edge_id[NORTH]].ex += iBlockSize;
+							world[i].edge_id[NORTH] = world[w].edge_id[NORTH];
+							world[i].edge_exist[NORTH] = true;
+						}
+						else {
+							sEdge edge;
+							edge.sx = x * iBlockSize; edge.sy = y * iBlockSize;
+							edge.ex = (x + 1) * iBlockSize; edge.ey = y * iBlockSize;
+
+							int edge_id = vEdge.size();
+							vEdge.push_back(edge);
+
+							world[i].edge_exist[NORTH] = true;
+							world[i].edge_id[NORTH] = edge_id;
+						}
+					}
+
+					// Check SOUTH SIDE
+					if (!world[s].exist) {
+						if (world[w].edge_exist[SOUTH]) {
+							// Increase bounder of the edge
+							vEdge[world[w].edge_id[SOUTH]].ex += iBlockSize;
+							world[i].edge_id[SOUTH] = world[w].edge_id[SOUTH];
+							world[i].edge_exist[SOUTH] = true;
+						}
+						else {
+							sEdge edge;
+							edge.sx = x * iBlockSize; edge.sy = (y + 1) * iBlockSize;
+							edge.ex = (x + 1) * iBlockSize; edge.ey = (y + 1) * iBlockSize;
+
+							int edge_id = vEdge.size();
+							vEdge.push_back(edge);
+
+							world[i].edge_exist[SOUTH] = true;
+							world[i].edge_id[SOUTH] = edge_id;
+						}
+					}
+
+				}
+			}
+		}
 	}
 
 private:
@@ -43,12 +181,16 @@ private:
 			}
 		}
 
-		FillCircle(GetMousePos(), 6, olc::WHITE);
+		for (int i = 0; i < vEdge.size(); i++) {
+			DrawLine(vEdge[i].sx, vEdge[i].sy, vEdge[i].ex, vEdge[i].ey);
+			FillCircle(vEdge[i].sx, vEdge[i].sy, 3, olc::RED);
+			FillCircle(vEdge[i].ex, vEdge[i].ey, 3, olc::RED);
+		}
 	}
 
 private:
 	sCell* world;
-	olc::vi2d header;
+	std::vector<sEdge> vEdge;
 	const int iWorldWidth = 40;
 	const int iWorldHeigh = 30;
 	const int iBlockSize = 16;
