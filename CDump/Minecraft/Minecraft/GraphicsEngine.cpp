91cca93c86d377aec58a381520d817d0df2034f1
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-05-09 00:24:02 +0300

    Clean code

--- CDump/Minecraft/Minecraft/GraphicsEngine.cpp
+++ CDump/Minecraft/Minecraft/GraphicsEngine.cpp
@@ -38,67 +38,38 @@ void GraphicsEngine::Construct(int32_t iHeight, int32_t iWidth) {
 void GraphicsEngine::Draw(olc::PixelGameEngine &GameEngine, float fElapsedTime) {
 	GameEngine.Clear(olc::BLACK);
 
-		Matrix4D matRotZ, matRotX;
+		Matrix4D mRotation, mTranslated;
 		fTheta += 1.0f * fElapsedTime;
 
-		// Rotation Z
-		matRotZ.MA[0][0] = cosf(fTheta);
-		matRotZ.MA[0][1] = sinf(fTheta);
-		matRotZ.MA[1][0] = -sinf(fTheta);
-		matRotZ.MA[1][1] = cosf(fTheta);
-		matRotZ.MA[2][2] = 1;
-		matRotZ.MA[3][3] = 1;
+		// Rotation ZX
+		mRotation = Matrix4D::RoutationOZ(fTheta) * Matrix4D::RoutationOX(fTheta * 0.5f);
 
-		// Rotation X
-		matRotX.MA[0][0] = 1;
-		matRotX.MA[1][1] = cosf(fTheta * 0.5f);
-		matRotX.MA[1][2] = sinf(fTheta * 0.5f);
-		matRotX.MA[2][1] = -sinf(fTheta * 0.5f);
-		matRotX.MA[2][2] = cosf(fTheta * 0.5f);
-		matRotX.MA[3][3] = 1;
+		// Translated
+		mTranslated = Matrix4D::Identity();
+		mTranslated.MA[3][2] = 3.0f;
 
 	for (auto& tr : mCube.tr) {
-		sTriangle trProjected, trTranslated, trRotatedZ, trRotatedZX;
+		sTriangle trProjected, trTranslated;
 
-		MultMatrix(tr.p[0], trRotatedZ.p[0], matRotZ);
-		MultMatrix(tr.p[1], trRotatedZ.p[1], matRotZ);
-		MultMatrix(tr.p[2], trRotatedZ.p[2], matRotZ);
+		trTranslated.p[0] = tr.p[0] * mRotation * mTranslated;
+		trTranslated.p[1] = tr.p[1] * mRotation * mTranslated;
+		trTranslated.p[2] = tr.p[2] * mRotation * mTranslated;
 
-
-		MultMatrix(trRotatedZ.p[0], trRotatedZX.p[0], matRotX);
-		MultMatrix(trRotatedZ.p[1], trRotatedZX.p[1], matRotX);
-		MultMatrix(trRotatedZ.p[2], trRotatedZX.p[2], matRotX);
-
-
-		trTranslated = trRotatedZX;
-		trTranslated.p[0].z += 3.0f;
-		trTranslated.p[1].z += 3.0f;
-		trTranslated.p[2].z += 3.0f;
 
 		sPoint3D normal, vect1, vect2;
 		vect1 = trTranslated.p[1] - trTranslated.p[0];
 		vect2 = trTranslated.p[2] - trTranslated.p[0];
+		normal = vect1.cross(vect2).normalize();
 
-		normal.x = vect1.y * vect2.z - vect1.z * vect2.y;
-		normal.y = vect1.z * vect2.x - vect1.x * vect2.z;
-		normal.z = vect1.x * vect2.y - vect1.y * vect2.x;
-		normal /=  sqrtf(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
-
-		sPoint3D vect3 = normal * (trTranslated.p[0] - vCamera);
-		if (vect3.x + vect3.y + vect3.z > 0.0f)
+		if (normal.prod(trTranslated.p[0] - vCamera) > 0.0f)
 			continue;
 			
 		sPoint3D light{ 0.0f, 0.0f, -1.0f };
-		light /= sqrtf(light.x * light.x + light.y * light.y + light.z * light.z);
+		int32_t color = (int32_t)(normal.prod(light.normalize()) * 255);
 
-		sPoint3D vect4 = normal * light;
-		int32_t color = (int32_t)((vect4.x + vect4.y + vect4.z) * 255);
-
-		MultMatrix(trTranslated.p[0], trProjected.p[0], mProjection);
-		MultMatrix(trTranslated.p[1], trProjected.p[1], mProjection);
-		MultMatrix(trTranslated.p[2], trProjected.p[2], mProjection);
-
-		trProjected.p[0] += 1.0f; trProjected.p[1] += 1.0f; trProjected.p[2] += 1.0f;
+		trProjected.p[0] = trTranslated.p[0] * mProjection + 1.0f;
+		trProjected.p[1] = trTranslated.p[1] * mProjection + 1.0f;
+		trProjected.p[2] = trTranslated.p[2] * mProjection + 1.0f;
 
 		trProjected.p[0].x *= 0.5f * (float)iScreenWidth; trProjected.p[0].y *= 0.5f * (float)iScreenHeight;
 		trProjected.p[1].x *= 0.5f * (float)iScreenWidth; trProjected.p[1].y *= 0.5f * (float)iScreenHeight;
@@ -111,12 +82,12 @@ void GraphicsEngine::Draw(olc::PixelGameEngine &GameEngine, float fElapsedTime) 
 			olc::Pixel(color, color, color)
 		);
 
-		GameEngine.DrawTriangle(
-			(int32_t)trProjected.p[0].x, (int32_t)trProjected.p[0].y,
-			(int32_t)trProjected.p[1].x, (int32_t)trProjected.p[1].y,
-			(int32_t)trProjected.p[2].x, (int32_t)trProjected.p[2].y,
-			olc::BLACK
-		);
+		//GameEngine.DrawTriangle(
+		//	(int32_t)trProjected.p[0].x, (int32_t)trProjected.p[0].y,
+		//	(int32_t)trProjected.p[1].x, (int32_t)trProjected.p[1].y,
+		//	(int32_t)trProjected.p[2].x, (int32_t)trProjected.p[2].y,
+		//	olc::BLACK
+		//);
 
 	}
 }
--- CDump/Minecraft/Minecraft/GraphicsEngine.h
+++ CDump/Minecraft/Minecraft/GraphicsEngine.h
@@ -1,19 +1,99 @@
 #pragma once
 #include "olcPixelGameEngine.h"
 
+struct Matrix4D {
+	float MA[4][4] = { 0 };
+
+	Matrix4D operator * (const Matrix4D& right) const {
+		Matrix4D res;
+		for (uint8_t i = 0; i < 4; i++)
+			for (uint8_t j = 0; j < 4; j++)
+				for (uint8_t k = 0; k < 4; k++)
+					res.MA[i][j] += this->MA[i][k] * right.MA[k][j];
+		return res;
+	}
+
+	static Matrix4D Identity() {
+		Matrix4D m;
+		m.MA[0][0] = 1.0f;
+		m.MA[1][1] = 1.0f;
+		m.MA[2][2] = 1.0f;
+		m.MA[3][3] = 1.0f;
+		return m;
+	}
+
+	static Matrix4D RoutationOZ(float angle) {
+		Matrix4D m = Identity();
+		m.MA[0][0] = cosf(angle);
+		m.MA[0][1] = sinf(angle);
+		m.MA[1][0] = -sinf(angle);
+		m.MA[1][1] = cosf(angle);
+		return m;
+	}
+
+	static Matrix4D RoutationOX(float angle) {
+		Matrix4D m = Identity();
+		m.MA[1][1] = cosf(angle);
+		m.MA[1][2] = sinf(angle);
+		m.MA[2][1] = -sinf(angle);
+		m.MA[2][2] = cosf(angle);
+		return m;
+	}
+
+	static Matrix4D RoutationOY(float angle) {
+		Matrix4D m = Identity();
+		m.MA[0][0] = cosf(angle);
+		m.MA[0][2] = sinf(angle);
+		m.MA[2][0] = -sinf(angle);
+		m.MA[2][2] = cosf(angle);
+		return m;
+	}
+};
 
 struct sPoint3D {
-	float x, y, z;
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+
+
+	float length() const { return sqrtf(this->x * this->x + this->y * this->y + this->z * this->z); }
+	sPoint3D normalize() const { return *this / length(); }
+	float prod(const sPoint3D& right) const { return this->x * right.x + this->y * right.y + this->z * right.z; }
+	sPoint3D cross(const sPoint3D& right) const { 
+		return {
+			this->y * right.z - this->z * right.y,
+			this->z * right.x - this->x * right.z,
+			this->x * right.y - this->y * right.x
+		}; 
+	}
 
 	sPoint3D operator + (const sPoint3D& right) const { return sPoint3D{ this->x + right.x, this->y + right.y, this->z + right.z }; }
+	sPoint3D operator + (const float& w) const { return sPoint3D{ this->x + w, this->y + w, this->z + w }; }
 	sPoint3D operator - (const sPoint3D& right) const { return sPoint3D{ this->x - right.x, this->y - right.y, this->z - right.z }; }
+	sPoint3D operator - (const float& w) const { return sPoint3D{ this->x - w, this->y - w, this->z - w }; }
 	sPoint3D operator * (const sPoint3D& right) const { return sPoint3D{ this->x * right.x, this->y * right.y, this->z * right.z }; }
+	sPoint3D operator * (const float& w) const { return sPoint3D{ this->x * w, this->y * w, this->z * w }; }
 	sPoint3D operator / (const sPoint3D& right) const { return sPoint3D{ this->x / right.x, this->y / right.y, this->z / right.z }; }
+	sPoint3D operator / (const float& w) const { return sPoint3D{ this->x / w, this->y / w, this->z / w }; }
 
-	sPoint3D& operator += (const float &w) { this->x += w; this->y += w; this->z += w; return *this; }
-	sPoint3D& operator -= (const float &w) { this->x -= w; this->y -= w; this->z -= w; return *this; }
-	sPoint3D& operator *= (const float &w) { this->x *= w; this->y *= w; this->z *= w; return *this; }
-	sPoint3D& operator /= (const float &w) { this->x /= w; this->y /= w; this->z /= w; return *this; }
+	sPoint3D& operator += (const float& w) { this->x += w; this->y += w; this->z += w; return *this; }
+	sPoint3D& operator += (const sPoint3D& right) { this->x += right.x; this->y += right.y; this->z += right.z; return *this; }
+	sPoint3D& operator -= (const float& w) { this->x -= w; this->y -= w; this->z -= w; return *this; }
+	sPoint3D& operator -= (const sPoint3D& right) { this->x -= right.x; this->y -= right.y; this->z -= right.z; return *this; }
+	sPoint3D& operator *= (const float& w) { this->x *= w; this->y *= w; this->z *= w; return *this; }
+	sPoint3D& operator *= (const sPoint3D& right) { this->x *= right.x; this->y *= right.y; this->z *= right.z; return *this; }
+	sPoint3D& operator /= (const float& w) { this->x /= w; this->y /= w; this->z /= w; return *this; }
+	sPoint3D& operator /= (const sPoint3D& right) { this->x /= right.x; this->y /= right.y; this->z /= right.z; return *this; }
+
+
+	sPoint3D operator * (const Matrix4D& MA) const {
+		float w = this->x * MA.MA[0][3] + this->y * MA.MA[1][3] + this->z * MA.MA[2][3] + MA.MA[3][3];
+		return sPoint3D{ 
+			this->x * MA.MA[0][0] + this->y * MA.MA[1][0] + this->z * MA.MA[2][0] + MA.MA[3][0],
+			this->x * MA.MA[0][1] + this->y * MA.MA[1][1] + this->z * MA.MA[2][1] + MA.MA[3][1],
+			this->x * MA.MA[0][2] + this->y * MA.MA[1][2] + this->z * MA.MA[2][2] + MA.MA[3][2]
+		} / (w != 0 ? w : 1.0f); 
+	}
 };
 
 
@@ -26,11 +106,6 @@ struct sField {
 };
 
 
-struct Matrix4D {
-	float MA[4][4] = { 0 };
-};
-
-
 class GraphicsEngine {
 public:
 	GraphicsEngine() {}
@@ -38,18 +113,6 @@ public:
 
 	void Construct(int32_t iHeight, int32_t iWidth);
 	void Draw(olc::PixelGameEngine &GameEngine, float fElapsedTime);
-
-
-private:
-	// Basic Function for Matrix Calculation
-	void MultMatrix(sPoint3D &iV, sPoint3D &oV, Matrix4D &MA) {
-		oV.x = iV.x * MA.MA[0][0] + iV.y * MA.MA[1][0] + iV.z * MA.MA[2][0] + MA.MA[3][0];
-		oV.y = iV.x * MA.MA[0][1] + iV.y * MA.MA[1][1] + iV.z * MA.MA[2][1] + MA.MA[3][1];
-		oV.z = iV.x * MA.MA[0][2] + iV.y * MA.MA[1][2] + iV.z * MA.MA[2][2] + MA.MA[3][2];
-		float w = iV.x * MA.MA[0][3] + iV.y * MA.MA[1][3] + iV.z * MA.MA[2][3] + MA.MA[3][3];
-
-		if (w != 0.0f) oV /= w;
-	}
 
 private:
 	// Projection Matrix
