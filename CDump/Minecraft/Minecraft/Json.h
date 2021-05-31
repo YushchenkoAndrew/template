b2b8a1584a1703d72a7803601f0a08c24c4273c2
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-06-01 02:55:19 +0300

    Swtich to std=c++17

--- CDump/Minecraft/Minecraft/Json.h
+++ CDump/Minecraft/Minecraft/Json.h
@@ -2,23 +2,42 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <any>
 
 class JSON {
+public:
+	enum nType {
+		INT,
+		FLOAT,
+		STRING,
+		UNDEFINED,
+		MAP
+	};
+
+	struct sVariable {
+		std::any value = 0;
+		nType type = JSON::INT;
+	};
+
+	typedef std::map<std::string, sVariable> json_t;
 
 private:
 	static void SkipComma(std::ifstream& iFile) {
 		if (cCurr != ',') {
-			printf("Expected \",\"");
+			printf("Expected \",\"\n");
 		}
 		iFile.get(cCurr);
 	}
 
 	static void SkipColon(std::ifstream& iFile) {
 		if (cCurr != ':') {
-			printf("Expected \":\"");
+			printf("Expected \":\"\n");
 		}
 		iFile.get(cCurr);
 	}
+
+	typedef std::map<std::string, sVariable> json_t;
 
 
 private:
@@ -26,19 +45,23 @@ private:
 		while ((cCurr == ' ' || cCurr == '\n' || cCurr == '\t' || cCurr == '\r') && iFile >> cCurr);
 	}
 
-	static void ParseString(std::ifstream& iFile) {
-		if (cCurr != '"') return;
+	static uint8_t ParseString(std::ifstream& iFile, sVariable& sVar) {
+		if (cCurr != '"') return 0u;
 		std::string sValue = "";
 		iFile.get(cCurr);
 		while (cCurr != '"' && !iFile.eof()) {
 			sValue += cCurr;
 			iFile.get(cCurr);
 		}
+		
+		sVar.type = JSON::STRING;
+		sVar.value = sValue;
+
 		iFile.get(cCurr);
-		printf("%s", sValue.c_str());
+		return 1u;
 	}
 
-	static void ParseNumber(std::ifstream& iFile) {
+	static uint8_t ParseNumber(std::ifstream& iFile, sVariable& sVar) {
 		char cSign = cCurr;
 		int32_t iNum = 0;
 		float fNum = 0.0f;
@@ -62,16 +85,28 @@ private:
 		}
 		
 		iNum *= cSign == '-' ? -1 : 1;
+		fNum *= iNum == 0 && cSign == '-' ? -1.0f : 1.0f;
 
-		if (iNum != 0 || fNum != 0.0f) printf("%d - %.5f", iNum, fNum);
+		if (iNum == 0 && fNum == 0.0f) return 0u;
+
+		if (fNum == 0.0f) {
+			sVar.type = JSON::INT;
+			sVar.value = iNum;
+			return 1u;
+		}
+
+		sVar.type = JSON::FLOAT;
+		sVar.value = (float)iNum + fNum;
+		return 1u;
 	}
 
-	static void ParseObject(std::ifstream& iFile) {
-		if (cCurr != '{') return;
+	static uint8_t ParseObject(std::ifstream& iFile, sVariable& sVar) {
+		if (cCurr != '{') return 0u;
 		iFile.get(cCurr);
 		SkipBlanks(iFile);
 
 		uint8_t bFirst = 1u;
+		json_t json;
 
 		while (cCurr != '}' && !iFile.eof()) {
 			if (!bFirst) {
@@ -79,44 +114,78 @@ private:
 				SkipBlanks(iFile);
 			}
 
-			printf("key = \"");
-			ParseString(iFile);
+			sVariable key;
+			ParseString(iFile, key);
 			SkipBlanks(iFile);
 			SkipColon(iFile);
 
-			printf("\" value = \"");
-			ParseValue(iFile);
-			printf("\"\n");
+			sVariable value;
+			ParseValue(iFile, value);
+
+			json[std::any_cast<std::string>(key.value)] = value;
 			bFirst = 0u;
 		}
 
+		sVar.type = JSON::MAP;
+		sVar.value = json;
+
 		iFile.get(cCurr);
+		return 1u;
 	}
 
-	static void ParseValue(std::ifstream& iFile) {
+	static void ParseValue(std::ifstream& iFile, sVariable& sVar) {
 		SkipBlanks(iFile);
 
-		ParseString(iFile);
-		ParseNumber(iFile);
-		ParseObject(iFile);
+		if (!ParseString(iFile, sVar) && !ParseNumber(iFile, sVar) && !ParseObject(iFile, sVar)) {
+			printf("Exception: Unknown type\n");
+		}
 
 		SkipBlanks(iFile);
 	}
 
 
 public:
-	static void parse(const std::string& sPath) {
+	static json_t parse(const std::string& sPath) {
 		std::ifstream iFile;
 		iFile.open(sPath);
-		if (!iFile.is_open()) {
-			printf("Unable to open file");
-			return;
-		}
+		//if (!iFile.is_open()) {
+		//	printf("Unable to open file");
+		//	return;
+		//}
 
+		sVariable json;
 		iFile.get(cCurr);
-		ParseValue(iFile);
+		ParseValue(iFile, json);
 
 		iFile.close();
+		return std::any_cast<json_t>(json.value);
+	}
+
+	static void Print(const json_t& json) {
+		printf("{\n");
+		for (auto& e : json) {
+			printf("%s : ", std::any_cast<std::string>(e.first).c_str());
+
+			switch (e.second.type) {
+				case JSON::INT: 
+					printf("%d,\n", std::any_cast<int32_t>(e.second.value));
+					break;
+
+				case JSON::FLOAT:
+					printf("%.5f,\n", std::any_cast<float>(e.second.value));
+					break;
+
+				case JSON::STRING:
+					printf("%s,\n", std::any_cast<std::string>(e.second.value).c_str());
+					break;
+
+				case JSON::MAP:
+					json_t child = std::any_cast<json_t>(e.second);
+					Print(child);
+					break;
+			}
+		}
+		printf("}\n");
 	}
 
 private:
