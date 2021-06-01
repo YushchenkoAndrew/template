6a61f14b0f03216a840df1c1e22aa06ae3a20af2
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-06-01 15:57:28 +0300

    Testing

--- CDump/Minecraft/Minecraft/Main.cpp
+++ CDump/Minecraft/Minecraft/Main.cpp
@@ -33,7 +33,7 @@ int main()
     //if (demo.Construct(250, 250, 4, 4))
     //    demo.Start();
 
-    JSON::json_t json = JSON::parse("Test.json");
-    JSON::Print(json);
+    JSON::parse("Test.json");
+    //JSON::Print(json);
     return 0;
 }
