22fb338b263895afb8cb4a31c35fd11d83516247
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-06-11 04:08:37 +0300

    Using enum instead of enum class, because I dont wanna to static_cast each time

--- CDump/Minecraft/Minecraft/MenuManager.h
+++ CDump/Minecraft/Minecraft/MenuManager.h
@@ -1,20 +1,24 @@
 #pragma once
 #include "Menu.h"
 
-enum class eMenuStates {
-	EDGE_EN = 17,
-	EDGE_DIS = 19,
-	SHADOW_EN = 33,
-	SHADOW_DIS = 35,
-	COLOR_EN = 49,
-	COLOR_DIS = 51,
-};
+namespace eMenuStates {
+	enum {
+		EDGE_EN = 17,
+		EDGE_DIS = 19,
+		SHADOW_EN = 33,
+		SHADOW_DIS = 35,
+		COLOR_EN = 49,
+		COLOR_DIS = 51,
+	};
+}
 
 class MenuManager {
 public:
 	void Init(const std::string& path) { cMenu.Load(path); cMenu.InitStates(mMenuState); }
 	void Open(Menu* pMenu) { Close(); stMenu.push_back(pMenu); isInUse = true; }
 	void Close() { stMenu.clear(); }
+
+	void Update(olc::PixelGameEngine& GameEngine);
 	void Draw(olc::PixelGameEngine& GameEngine, std::unique_ptr<olc::Decal>& decMenu, olc::vi2d vOffset, float& fTime);
 
 	sMenuState& GetState(int32_t nId) { return mMenuState[STATE_GROUP(nId)][STATE_INDEX(nId)]; }
@@ -22,7 +26,6 @@ public:
 
 private:
 	void OnMove(olc::PixelGameEngine& GameEngine);
-	void UpdateState();
 	void OnConfirm();
 
 private:
