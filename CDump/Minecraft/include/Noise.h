733c9f9a5c9ea0d95f61efb3bff33a99cf176b0b
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-06-19 03:56:56 +0300

    Create PerlinNoise, FractalNoise methods

--- CDump/Minecraft/include/Noise.h
+++ CDump/Minecraft/include/Noise.h
@@ -2,67 +2,102 @@
 #include <stdint.h>
 #include <cmath>
 
-struct sVect2D {
-	float x, y;
+struct sPoint2D {
+	float x = 0.0f;
+	float y = 0.0f;
+
+	static inline sPoint2D normalize(const sPoint2D& p) { return p / sPoint2D::length(p); }
+	static inline float length(const sPoint2D& p) { return sqrtf(p.x * p.x + p.y * p.y); }
+	static inline float dot(const sPoint2D& right, const sPoint2D& left) { return right.x * left.x + right.y * left.y; }
+
+	inline float dot(const sPoint2D& right) const { return this->x * right.x + this->y * right.y; }
+
+	sPoint2D operator + (const sPoint2D& right) const { return sPoint2D{ this->x + right.x, this->y + right.y }; }
+	sPoint2D operator - (const sPoint2D& right) const { return sPoint2D{ this->x - right.x, this->y - right.y }; }
+	sPoint2D operator / (const float& w) const { return sPoint2D{ this->x / w, this->y / w }; }
 };
 
-class Random {
+class Noise {
 public:
-
-	Random(): nHeight(1), nWidth(1) {}
-	Random(int32_t height, int32_t width): nHeight(height), nWidth(width) {}
-
 
 	// Random number generator
 	// https://lemire.me/blog/2019/03/19/the-fastest-conventional-random-number-generator-that-can-pass-big-crush/
-	uint32_t rand() {
-		nRandState += 0xe120fc15;
+	static uint32_t rand() {
+		GetRandState() += 0xe120fc15;
 		uint64_t tmp;
-		tmp = (uint64_t)nRandState * 0x4a39b70d;
-		uint32_t m1 = (tmp >> 32) ^ tmp;
+		tmp = (uint64_t)GetRandState() * 0x4a39b70d;
+		uint32_t m1 = (uint32_t)((tmp >> 32) ^ tmp);
 		tmp = (uint64_t)m1 * 0x12fad5c9;
-		uint32_t m2 = (tmp >> 32) ^ tmp;
+		uint32_t m2 = (uint32_t)((tmp >> 32) ^ tmp);
 		return m2;
 	}
 
+	static float PerlinNoise(float x, float y) {
+		sPoint2D p{ x, y };
+		return PerlinNoise(p);
+	}
 
 	// Perlin Noise
 	// https://gpfault.net/posts/perlin-noise.txt.html
-	float Noise(float x, float y) {
-		float x0 = floorf(x); float y0 = floorf(y);
-		float x1 = x0 + 1.0f; float y1 = y0 + 0.0f;
-		float x2 = x0 + 0.0f; float y2 = y0 + 1.0f;
-		float x3 = x0 + 1.0f; float y3 = y0 + 1.0f;
+	static float PerlinNoise(const sPoint2D& p) {
+		sPoint2D p0{ floorf(p.x), floorf(p.y) };
+		sPoint2D p1 = p0 + sPoint2D{ 1.0f, 0.0f };
+		sPoint2D p2 = p0 + sPoint2D{ 0.0f, 1.0f };
+		sPoint2D p3 = p0 + sPoint2D{ 1.0f, 1.0f };
 
-		
 		// Look up gradients at lattice points
-		sVect2D g0 = grad(x0, y0);
-		sVect2D g1 = grad(x1, y1);
-		sVect2D g2 = grad(x2, y2);
-		sVect2D g3 = grad(x3, y3);
+		sPoint2D g0 = grad(p0);
+		sPoint2D g1 = grad(p1);
+		sPoint2D g2 = grad(p2);
+		sPoint2D g3 = grad(p3);
 
-		float t0 = x - x0;
+		float t0 = p.x - p0.x;
 		float fade_t0 = fade(t0);
 
-		float t1 = x - x1;
+		float t1 = p.y - p0.y;
 		float fade_t1 = fade(t1);
 
-		// TODO:
-		//float p0p1 = (1.0f - fade_t0) * dot(g0, )
+		float p0p1 = (1.0f - fade_t0) * sPoint2D::dot(g0, p - p0) + fade_t0 * sPoint2D::dot(g1, p - p1);
+		float p2p3 = (1.0f - fade_t0) * sPoint2D::dot(g2, p - p2) + fade_t0 * sPoint2D::dot(g3, p - p3);
+
+		return (1.0f - fade_t1) * p0p1 + fade_t1 * p2p3;
+	}
+
+	static float FractalNoise(float x, float y) {
+		sPoint2D p{ x, y };
+		return
+			PerlinNoise(p / 64.0f) * 1.0f +
+			PerlinNoise(p / 32.0f) * 0.5f +
+			PerlinNoise(p / 16.0f) * 0.25f +
+			PerlinNoise(p / 8.0f) * 0.125f;
 	}
 
 
 private:
-	inline float fade(float t) { return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f); }
+	static inline float fade(float t) { return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f); }
 
-	sVect2D grad(float x, float y) {
-		// TODO:
+	static sPoint2D grad(const sPoint2D& p) {
+		float random = 2920.0f * sinf(p.x * 21942.0f + p.y * 171324.0f + 8912.0f) * cosf(p.x * 23157.0f * p.y * 217832.0f + 9758.0f);
+		return sPoint2D::normalize({ cosf(random), sinf(random) });
 	}
 
 
-private:
-	int32_t nHeight;
-	int32_t nWidth;
+	static inline uint32_t& GetRandState() {
+		static uint32_t nRandState = 0u;
+		return nRandState;
+	}
+};
 
-	uint32_t nRandState = 0u;
+
+// Some envelopes
+struct TrueNoise {
+	static inline float Noise(float, float) { return 1.0f; }
+};
+
+struct PerlinNoise {
+	static inline float Noise(float x, float y) { return (Noise::PerlinNoise(x, y) + 1.0f) / 2.0f; }
+};
+
+struct FractalNoise {
+	static inline float Noise(float x, float y) { return (Noise::FractalNoise(x, y) + 1.0f) / 2.0f; }
 };
