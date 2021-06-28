822f05fa3161c9cc0e78dae356e0a0cc8f3fe581
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-06-28 04:36:53 +0300

    Finish basic impl of Animated

--- CDump/Minecraft/src/lua/Animated.lua
+++ CDump/Minecraft/src/lua/Animated.lua
@@ -3,9 +3,6 @@ require("src/lua/Json")
 -- Init Object
 Animated = {
   sprites = {},
-
-  frame = 0,
-  frameTime = 0.0,
 }
 
 function Animated:Init(path)
@@ -18,9 +15,16 @@ function Animated:Init(path)
       state.NextFrame = coroutine.create(Animated.NextFrame)
       coroutine.resume(state.NextFrame, Animated, 0.0, key, name)
     end
+
+    sprites.NextState = coroutine.create(Animated.NextState)
+    coroutine.resume(sprites.NextState, Animated, 0.0, key)
   end
 end
 
+-- TODO:
+function Animated:LoadSpritePtr(sName, pSprite)
+
+end
 
 function Animated:NextFrame(fElapsedTime, sName, sState)
 
@@ -36,6 +40,7 @@ function Animated:NextFrame(fElapsedTime, sName, sState)
       stMachine.time = 0.0
 
       while true do
+        -- FIXME: Change with returning ptr
         class, fElapsedTime = coroutine.yield(key)
 
         stMachine.time = stMachine.time + fElapsedTime
@@ -44,32 +49,66 @@ function Animated:NextFrame(fElapsedTime, sName, sState)
     end
   until stMachine.mode ~= "LOOP"
 
-  -- TODO: Do correct exit
+  return nil
 end
 
--- TODO: Create StateMachine
-function Animated:GetFrame(fElapsedTime, sName, sState)
-  if (coroutine.status(Animated.sprites[sName]["states"][sState].NextFrame) == "dead") then
-    -- Animated.sprites[sName].FrameThread = coroutine.create(Animated.NextFrame)
-    return -1
-  end
 
-  return coroutine.resume(Animated.sprites[sName]["states"][sState].NextFrame, Animated, fElapsedTime, sName, sState)
+function Animated:NextState(fElapsedTime, sName)
+
+  -- Init StateMachine
+  local stMachine = Animated.sprites[sName]["states"]
+  local status = nil
+  local frame = nil
+  local class = nil
+  class, fElapsedTime = coroutine.yield()
+
+  repeat
+    for key, state in pairs(stMachine) do
+      if coroutine.status(state.NextFrame) ~= "dead" then
+        status, frame = coroutine.resume(state.NextFrame, Animated, fElapsedTime, sName, key)
+        if frame ~= nil then class, fElapsedTime = coroutine.yield(frame) end
+      elseif state.next then
+        -- Create and init value of coroutine
+        state.NextFrame = coroutine.create(Animated.NextFrame)
+        coroutine.resume(state.NextFrame, Animated, fElapsedTime, sName, key)
+
+        -- Get new values
+        status, frame = coroutine.resume(state.NextFrame, Animated, fElapsedTime, sName, key)
+        if frame ~= nil then class, fElapsedTime = coroutine.yield(frame) end
+      else status = true; break end
+
+      status = true
+      if frame == nil and not state.next then break end
+      status = false
+    end
+  until status
+
+  return nil
 end
 
-function Animated:SetState(state)
-  Animated.currState = state
-  Animated.frame = 0
+
+function Animated:GetFrame(fElapsedTime, sName)
+  local stMachine = Animated.sprites[sName].NextState
+  if (coroutine.status(stMachine) == "dead") then return nil end
+
+  local status, frame = coroutine.resume(stMachine, Animated, fElapsedTime, sName)
+  return frame
+end
+
+
+-- TODO: this method on demand
+function Animated:GetFrameByState()
+
 end
 
 Animated:Init("assets/AnimatedSprite.json")
-print(Animated:GetFrame(0.0, "MenuCursor", "click"))
-print(Animated:GetFrame(1.0, "MenuCursor", "click"))
-print(Animated:GetFrame(2.0, "MenuCursor", "click"))
-print(Animated:GetFrame(3.0, "MenuCursor", "click"))
-print(Animated:GetFrame(4.0, "MenuCursor", "click"))
-print(Animated:GetFrame(5.0, "MenuCursor", "click"))
-print(Animated:GetFrame(6.0, "MenuCursor", "click"))
+print(Animated:GetFrame(0.0, "MenuCursor"))
+print(Animated:GetFrame(1.0, "MenuCursor"))
+print(Animated:GetFrame(2.0, "MenuCursor"))
+print(Animated:GetFrame(3.0, "MenuCursor"))
+print(Animated:GetFrame(4.0, "MenuCursor"))
+print(Animated:GetFrame(5.0, "MenuCursor"))
+print(Animated:GetFrame(6.0, "MenuCursor"))
 
 -- local thread = coroutine.create(Animated.GetFrame)
 -- print(coroutine.resume(thread, "Animated", 0.0, "MenuCursor"))
