112e337bc0e760f5050732061cb21899a014c1dc
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-07-01 02:48:35 +0300

    Start working on Set/Reset block fuctionality

--- CDump/Minecraft/src/Minecraft.cpp
+++ CDump/Minecraft/src/Minecraft.cpp
@@ -1,10 +1,39 @@
 #include "Minecraft.h"
 
-void sChunk::LoadMap(std::vector<sTriangle>& vMap) {
+void sChunk::LoadMap(std::vector<sBlock*>& vpBlock) {
 	for (auto& block : vBlock) {
-		block.LoadMap(vMap);
+		block.Update();
+		//if (IS_EXIST(block.bStatus)) vpBlock.push_back(std::make_unique<sBlock>(block));
+		vpBlock.push_back(&block);
 	}
+	
 }
+
+void sChunk::SetBlock(int32_t x, int32_t y, int32_t z) {
+	vBlock[CHUNK_INDEX(x, y, z)].bStatus |= EXIST_MASK;
+
+	if (x != 0 && IS_EXIST(vBlock[CHUNK_INDEX(x - 1, y, z)].bStatus)) {
+		vBlock[CHUNK_INDEX(x - 1, y, z)].bStatus |= EAST_MASK;
+		vBlock[CHUNK_INDEX(x, y, z)].bStatus |= WEST_MASK;
+	}
+
+	if (y != 0 && IS_EXIST(vBlock[CHUNK_INDEX(x, y - 1, z)].bStatus)) {
+		vBlock[CHUNK_INDEX(x, y - 1, z)].bStatus |= UP_MASK;
+		vBlock[CHUNK_INDEX(x, y, z)].bStatus |= DOWN_MASK;
+	}
+
+	if (z != 0 && IS_EXIST(vBlock[CHUNK_INDEX(x, y, z - 1)].bStatus)) {
+		vBlock[CHUNK_INDEX(x, y, z - 1)].bStatus |= SOUTH_MASK;
+		vBlock[CHUNK_INDEX(x, y, z)].bStatus |= NORTH_MASK;
+	}
+
+	vBlock[CHUNK_INDEX(x, y, z)].Update();
+}
+
+
+void sChunk::ResetBlock(int32_t x, int32_t y, int32_t z) {
+}
+
 
 
 
@@ -17,7 +46,9 @@ void Minecraft::Init(int32_t iHeight, int32_t iWidth, LuaScript& luaConfig) {
 	cEngine3D.Init(iHeight, iWidth, luaConfig);
 
 	InitMap(Type2Type<FractalNoise>());
-	for (auto& chunk : vChunk) chunk.LoadMap(cEngine3D.trMap);
+	for (auto& chunk : vChunk) chunk.LoadMap(cEngine3D.vpBlocks);
+
+	SetBlock(25, CHUNK_SIZE - 1, 1);
 }
 
 void Minecraft::Update(olc::PixelGameEngine& GameEngine, MenuManager& mManager, float& fElapsedTime) {
@@ -30,27 +61,26 @@ void Minecraft::Update(olc::PixelGameEngine& GameEngine, MenuManager& mManager, 
 
 
 	// TEMP:
-	sBlock blCamera;
-	blCamera.SetPos(cEngine3D.cCamera.GetPos());
+	//sBlock blCamera;
+	//blCamera.SetPos(cEngine3D.cCamera.GetPos());
 	//blCamera.vPos.z += 5.0f;
 
-	for (int32_t x = -1; x < 2; x++) {
-		for (int32_t y = -1; y < 2; y++) {
-			for (int32_t z = -1; z < 2; z++) {
-				sBlock blNext = vChunk[0].GetBlock((int32_t)blCamera.vPos.x + x, (int32_t)blCamera.vPos.y + y, (int32_t)blCamera.vPos.z + z);
+	//for (int32_t x = -1; x < 2; x++) {
+	//	for (int32_t y = -1; y < 2; y++) {
+	//		for (int32_t z = -1; z < 2; z++) {
+	//			sBlock blNext = vChunk[0].GetBlock((int32_t)blCamera.vPos.x + x, (int32_t)blCamera.vPos.y + y, (int32_t)blCamera.vPos.z + z);
 
-				if (BlockCollision::IsCollide(blCamera, blNext)) {
-					printf("Collide - (%d, %d, %d)\n", (int32_t)blCamera.vPos.x + x, (int32_t)blCamera.vPos.y + y, (int32_t)blCamera.vPos.z + z);
+	//			if (BlockCollision::IsCollide(blCamera, blNext)) {
+	//				printf("Collide - (%d, %d, %d)\n", (int32_t)blCamera.vPos.x + x, (int32_t)blCamera.vPos.y + y, (int32_t)blCamera.vPos.z + z);
 
-					blNext.bStatus |= EXIST_MASK;
-					blNext.bStatus &= ~(DOWN_MASK | UP_MASK | WEST_MASK | EAST_MASK | SOUTH_MASK | NORTH_MASK);
-					blNext.LoadMap(cEngine3D.trMap);
-				}
-			}
-		}
-	}
+	//				blNext.bStatus |= EXIST_MASK;
+	//				blNext.bStatus &= ~(DOWN_MASK | UP_MASK | WEST_MASK | EAST_MASK | SOUTH_MASK | NORTH_MASK);
+	//				blNext.LoadMap(cEngine3D.trMap);
+	//			}
+	//		}
+	//	}
+	//}
 }
-
 
 void Minecraft::Draw(olc::PixelGameEngine& GameEngine, MenuManager& mManager) {
 	cEngine3D.Draw(GameEngine, mManager);
@@ -66,3 +96,19 @@ void Minecraft::DrawNoise(olc::PixelGameEngine& GameEngine) {
 		}
 	}
 }
+
+
+
+void Minecraft::SetBlock(int32_t x, int32_t y, int32_t z) {
+	int32_t index = MapIndex(x / CHUNK_SIZE, z / CHUNK_SIZE);
+	if (index >= vChunk.size() || y > CHUNK_SIZE) return;
+	vChunk[index].SetBlock(x, y, z);
+}
+
+
+void Minecraft::ResetBlock(int32_t x, int32_t y, int32_t z) {
+	int32_t index = MapIndex(x / CHUNK_SIZE, z / CHUNK_SIZE);
+	if (index >= vChunk.size() || y > CHUNK_SIZE) return;
+	vChunk[index].ResetBlock(x, y, z);
+}
+
