b527f7643890cae3d0b5f5c237c5e801472db6ee
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-07-22 03:01:16 +0300

    Add some changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
+import "../styles/globals.css";
+import "../styles/bootstrap.min.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
