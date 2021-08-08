5676c76354ec43cd1026bdacdf5acd7e6e60d7e0
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-08-08 17:54:26 +0300

    Add card component

---
+++ components/Card.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+import styles from "./Card.module.css";
+
+export interface CardProps {
+  img: string;
+  title: string;
+  href: string;
+  size?: string;
+  color?: string;
+  description?: string;
+  children?: React.ReactNode;
+}
+
+export default function Card(props: CardProps) {
+  return (
+    <div className="card card-has-bg click-col">
+      <Image
+        className="card-img"
+        src={props.img}
+        alt={`Project: ${props.title}`}
+      />
+      <a
+        className={`card-img-overlay d-flex flex-column ${
+          props.color ?? "text-white"
+        }`}
+        href={props.href}
+      >
+        <div className="card-body">
+          <h4
+            className={`${styles["card-title"]} ${
+              styles[props.size ?? "title-md"]
+            }`}
+          >
+            {props.title}
+          </h4>
+          <small>{props.description}</small>
+        </div>
+        <div className="card-footer">
+          <div className="media">
+            {/* TODO: Maybe add footer info such as github or other stuff */}
+            {props.children}
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
