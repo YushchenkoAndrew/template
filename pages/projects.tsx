41a5964cfb4d7369c43ec79b1878cc8982e93773
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-08-20 19:23:32 +0300

    Change some useless stuff

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -69,7 +69,7 @@ export default function Projects() {
 
           {/* WARNING: Temp file */}
           <Card
-            img="/projects/img/Black.webp"
+            img="https://images.unsplash.com/photo-1457976326363-73a4b5fb9e79?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8dGVjaCxzdHJlZXR8fHx8fHwxNjI4NDI3MzEy&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=600"
             title="Some text"
             href="#"
             size="title-lg"
