997c40cf4f35500fe68eba89f5b91d9ebc6702b3
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-08-27 16:29:37 +0300

    Set caching time to client

--- next.config.js
+++ next.config.js
@@ -2,4 +2,27 @@ module.exports = {
   reactStrictMode: true,
   webpack5: true,
   basePath: "/projects",
+
+  async headers() {
+    return [
+      {
+        source: "/api/info/analytics",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2000, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/api/info/statistic",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2000, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
