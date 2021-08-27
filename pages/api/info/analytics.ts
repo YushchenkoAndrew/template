e7564a515401f8af01fd5de48158821c1a4a58e3
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-08-27 16:29:06 +0300

    Start working on api request to back

--- pages/api/info/analytics.ts
+++ pages/api/info/analytics.ts
@@ -1,44 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { DefaultRes, Stat } from "../ping";
 import redis from "../../../config/redis";
+import { formatDate } from "../../info";
+import { ApiReq, InfoData } from "../../../types/api";
+import { AnalyticsData, DefaultRes, Stat } from "../../../types/request";
+import { Analytics } from "../../../types/info";
+
+const apiURL =
+  process.env.NODE_ENV == "production"
+    ? `${process.env.API_HOST}:${process.env.API_PORT}`
+    : "localhost:31337";
 
 type QueryParams = { date: string };
 
-export type Analytics = { ctr: number; cr_media: number; cr_projects: number };
-export type Data = {
-  stat: Stat;
-  doughnut: Analytics;
-  line: number[];
-  days: string[];
-};
+type TimeLine = { time: string; value: number };
 
-type ApiReq = {
-  items: number;
-  result: (InfoSum | InfoData)[];
-  status: Stat;
-  totalItems: number;
-};
-
-type InfoData = {
-  ID: number;
-  CreatedAt: string;
-  Countries: string;
-  Views: number;
-  Clicks: number;
-  Media: number;
-  Visitors: number;
-};
-
-type InfoSum = {
-  Views: number;
-  Clicks: number;
-  Media: number;
-  Visitors: number;
-};
+function FormData(doughnut: Analytics, lineData: TimeLine[]): AnalyticsData {
+  return {
+    stat: "OK",
+    doughnut,
+    line: lineData.map((item) => item.value),
+    days: lineData.map((item) => item.time),
+  };
+}
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | DefaultRes>
+  res: NextApiResponse<AnalyticsData | DefaultRes>
 ) {
   if (req.method !== "GET") {
     return res
@@ -53,96 +40,102 @@ export default function handler(
       .json({ stat: "ERR", message: "Not all Query params are declared!" });
   }
 
-  const host = process.env.API_HOST ?? "";
-  const port = process.env.API_PORT ?? "";
-  if (host == "") {
-    return res.status(500).json({
-      stat: "ERR",
-      message: "API HOST name is not specified",
-    });
-  }
+  const now = formatDate(new Date());
 
-  Promise.all([
-    new Promise((resolve, reject) => {
-      redis.hgetall("Info:Sum", (err, reply) => {
-        if (!err && reply) {
-          return resolve({
-            ctr: +reply.Views ? +reply.Clicks / +reply.Views : 1,
-            cr_media: +reply.Visitors ? +reply.Media / +reply.Visitors : 1,
-            cr_projects: +reply.Visitors ? +reply.Clicks / +reply.Visitors : 1,
-          });
+  redis.get("Info:Day", (err, prevDate) => {
+    Promise.all([
+      new Promise((resolve, reject) => {
+        redis.hgetall("Info:Sum", (err, reply) => {
+          if (!err && reply) {
+            return resolve({
+              ctr: +reply.Views ? +reply.Clicks / +reply.Views : 1,
+              cr_media: +reply.Visitors ? +reply.Media / +reply.Visitors : 1,
+              cr_projects: +reply.Visitors
+                ? +reply.Clicks / +reply.Visitors
+                : 1,
+            });
+          }
+
+          fetch(`http://${apiURL}/api/info/sum`)
+            .then((res) => res.json())
+            .then((res: ApiReq) => {
+              const result = res.result.pop() as InfoData;
+              if (!res.items || !result || res.status == "ERR")
+                return reject("Idk some thing wrong with back-end");
+
+              redis.hmset("Info:Sum", {
+                Clicks: result.Clicks.toString(),
+                Media: result.Media.toString(),
+                Views: result.Views.toString(),
+                Visitors: result.Visitors.toString(),
+              });
+
+              // TODO: Cron job to send update to back
+
+              return resolve({
+                ctr: result.Views ? result.Clicks / result.Views : 1,
+                cr_media: result.Visitors ? result.Media / result.Visitors : 1,
+                cr_projects: result.Visitors
+                  ? result.Media / result.Visitors
+                  : 1,
+              });
+            })
+            .catch((err) => reject(err));
+        });
+      }),
+      new Promise((resolve, reject) => {
+        redis.get("Info:Days", (err, reply) => {
+          redis.set("Info:Day", date);
+          if (!err && reply && prevDate == date)
+            return resolve(JSON.parse(reply));
+
+          let prev = new Date(date);
+          prev.setDate(prev.getDate() - 7);
+
+          fetch(
+            `http://${apiURL}/api/info/range?end=${date}&start=${formatDate(
+              prev
+            )}&orderBy=CreatedAt`
+          )
+            .then((res) => res.json())
+            .then((res: ApiReq) => {
+              if (!res.items || res.status == "ERR")
+                return reject("Idk some thing wrong with back-end");
+
+              let result = (res.result as InfoData[]).map((item) => ({
+                time: item.CreatedAt.split("T")[0],
+                value: item.Visitors,
+              }));
+
+              redis.set("Info:Days", JSON.stringify(result));
+              redis.expire("Info:Days", 2 * 60 * 60);
+              return resolve(result);
+            })
+            .catch((err) => reject(err));
+        });
+      }),
+    ])
+      .then((results) => {
+        if (now != date) {
+          return res
+            .status(200)
+            .json(FormData(results[0] as Analytics, results[1] as TimeLine[]));
         }
 
-        // FIXME:
-        // fetch(`${host}${port == "" ? ":" + port : ""}/api/info/sum`)
-        fetch("http://localhost:31337/api/info/sum")
-          .then((res) => res.json())
-          .then((res: ApiReq) => {
-            const result = res.result.pop();
-            if (!res.items || !result)
-              return reject("Idk some thing wrong with back-end");
-
-            redis.hmset("Info:Sum", {
-              Clicks: result.Clicks.toString(),
-              Media: result.Media.toString(),
-              Views: result.Views.toString(),
-              Visitors: result.Visitors.toString(),
+        redis.hgetall("Info:Sum", (err, reply) => {
+          if (!err && reply) {
+            (results[1] as TimeLine[]).pop();
+            (results[1] as TimeLine[]).push({
+              time: now,
+              value: +reply.Visitors,
             });
+          }
 
-            return resolve({
-              ctr: result.Views ? result.Clicks / result.Views : 1,
-              cr_media: result.Visitors ? result.Media / result.Visitors : 1,
-              cr_projects: result.Visitors ? result.Media / result.Visitors : 1,
-            });
-          })
-          .catch((err) => reject(err));
-      });
-    }),
-    new Promise((resolve, reject) => {
-      redis.get("Info:Days", (err, reply) => {
-        if (!err && reply) return resolve(JSON.parse(reply));
-
-        // FIXME: Change url to get data from range days
-        // fetch(`${host}${port == "" ? ":" + port : ""}/api/info`)
-        fetch("http://localhost:31337/api/info")
-          .then((res) => res.json())
-          .then((res: ApiReq) => {
-            console.log(res);
-            if (!res.items) return reject("Idk some thing wrong with back-end");
-
-            let result = (res.result as InfoData[]).map((item) => ({
-              time: item.CreatedAt.split("T")[0],
-              value: item.Visitors,
-            }));
-
-            // TODO: Add updated visitors for curr date
-            // result.push([ ])
-
-            redis.set("Info:Days", JSON.stringify(result));
-            return resolve(result);
-          })
-          .catch((err) => reject(err));
-      });
-    }),
-  ])
-    .then((results) => {
-      console.log(results);
-
-      res.status(200).json({
-        stat: "OK",
-        doughnut: results[0] as Analytics,
-        line: [12, 19, 3, 5, 2, 3, 5],
-        days: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Orange"],
-      });
-    })
-    .catch((err) => console.log(err));
-
-  // TODO: Get data from API
-  console.log(date);
-  // res.status(200).json({
-  //   stat: "OK",
-  //   doughnut: { ctr: 10, cr_media: 3, cr_projects: 5 },
-  //   line: [12, 19, 3, 5, 2, 3, 5],
-  //   days: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Orange"],
-  // });
+          res
+            .status(200)
+            .json(FormData(results[0] as Analytics, results[1] as TimeLine[]));
+        });
+      })
+      .catch((err) => console.log(err));
+  });
 }
--- pages/api/info/statistic.ts
+++ pages/api/info/statistic.ts
@@ -1,21 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { DefaultRes, Stat as StatMessage } from "../ping";
+import redis from "../../../config/redis";
+import { ApiReq, WorldData } from "../../../types/api";
+import { Country, StatInfo } from "../../../types/info";
+import { DefaultRes, StatisticData } from "../../../types/request";
+
+const apiURL =
+  process.env.NODE_ENV == "production"
+    ? `${process.env.API_HOST}:${process.env.API_PORT}`
+    : "localhost:31337";
 
 type QueryParams = { date: string };
-
-export type Country = { country: string; value: number };
-export type Stat = { value: number; gain: number };
-export type StatInfo = { users: Stat; views: Stat; countries: number };
-
-export type Data = {
-  stat: StatMessage;
-  info: StatInfo;
-  map: Country[];
-};
-
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | DefaultRes>
+  res: NextApiResponse<StatisticData | DefaultRes>
 ) {
   if (req.method !== "GET") {
     return res
@@ -30,27 +27,101 @@ export default function handler(
       .json({ stat: "ERR", message: "Not all Query params are declared!" });
   }
 
+  Promise.all([
+    new Promise((resolve, reject) => {
+      redis.hgetall("Info:Stat", (err, reply) => {
+        if (!err && reply) {
+          return resolve({
+            users: { value: +reply.userValue, gain: +reply.userGain },
+            views: { value: +reply.viewsValue, gain: +reply.viewsGain },
+            countries: +reply.countries,
+          });
+        }
+
+        fetch(`http://${apiURL}/api/info/sum`)
+          .then((res) => res.json())
+          .then((res: ApiReq) => {
+            const result = res.result.pop();
+            if (!res.items || !result || res.status == "ERR")
+              return reject("Idk some thing wrong with back-end");
+
+            // TODO:
+            // redis.hmset("Info:Sum", {
+            //   Clicks: result.Clicks.toString(),
+            //   Media: result.Media.toString(),
+            //   Views: result.Views.toString(),
+            //   Visitors: result.Visitors.toString(),
+            // });
+
+            // TODO: Cron job to send update to back
+
+            // FIXME:
+            return resolve({
+              users: { value: 25, gain: -64 },
+              views: { value: 155, gain: 64 },
+              countries: 55,
+            });
+          })
+          .catch((err) => reject(err));
+      });
+    }),
+    new Promise((resolve, reject) => {
+      redis.get("Info:Country", (err, reply) => {
+        if (!err && reply) return resolve(JSON.parse(reply));
+
+        let prev = new Date(date);
+        prev.setDate(prev.getDate() - 7);
+
+        fetch(`http://${apiURL}/api/world`)
+          .then((res) => res.json())
+          .then((res: ApiReq) => {
+            console.log(res);
+            if (!res.items || res.status == "ERR")
+              return reject("Idk some thing wrong with back-end");
+
+            let result = (res.result as WorldData[]).map((item) => ({
+              country: item.Country,
+              value: item.Visitors,
+            }));
+
+            // redis.set("Info:Days", JSON.stringify(result));
+            // redis.expire("Info:Days", 2 * 60 * 60);
+            return resolve(result);
+          })
+          .catch((err) => reject(err));
+      });
+    }),
+  ])
+    .then((results) => {
+      res.status(200).json({
+        stat: "OK",
+        info: results[0] as StatInfo,
+        map: results[1] as Country[],
+      });
+    })
+    .catch((err) => console.log(err));
+
   // TODO: Get data from API
   console.log(date);
 
-  res.status(200).json({
-    stat: "OK",
-    info: {
-      users: { value: 25, gain: -64 },
-      views: { value: 155, gain: 64 },
-      countries: 55,
-    },
-    map: [
-      { country: "cn", value: 1389618778 },
-      { country: "in", value: 1311559204 },
-      { country: "us", value: 331883986 },
-      { country: "id", value: 264935824 },
-      { country: "pk", value: 210797836 },
-      { country: "br", value: 210301591 },
-      { country: "ng", value: 208679114 },
-      { country: "bd", value: 161062905 },
-      { country: "ru", value: 141944641 },
-      { country: "mx", value: 127318112 },
-    ],
-  });
+  // res.status(200).json({
+  //   stat: "OK",
+  //   info: {
+  //     users: { value: 25, gain: -64 },
+  //     views: { value: 155, gain: 64 },
+  //     countries: 55,
+  //   },
+  //   map: [
+  //     { country: "cn", value: 1389618778 },
+  //     { country: "in", value: 1311559204 },
+  //     { country: "us", value: 331883986 },
+  //     { country: "id", value: 264935824 },
+  //     { country: "pk", value: 210797836 },
+  //     { country: "br", value: 210301591 },
+  //     { country: "ng", value: 208679114 },
+  //     { country: "bd", value: 161062905 },
+  //     { country: "RU", value: 141944641 },
+  //     { country: "mx", value: 127318112 },
+  //   ],
+  // });
 }
