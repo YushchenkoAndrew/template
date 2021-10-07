6d20056418deef1024356a6e4f4e4a811bb72e76
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-10-07 17:09:40 +0300

    Use async

--- pages/api/view/ping.ts
+++ pages/api/view/ping.ts
@@ -2,13 +2,23 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import redis from "../../../config/redis";
 import md5 from "../../../lib/md5";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+function CheckUser(id: string) {
+  return new Promise<boolean>((resolve, reject) => {
+    redis.get(id.toString(), (err, ok) => {
+      if (err || !ok) resolve(false);
+      resolve(true);
+    });
+  });
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "HEAD") return res.status(405).send("");
-  const id = req.headers["x-custom-header"];
+  const id = req.headers["x-custom-header"] as string;
   if (!id) return res.status(400).send("");
 
-  redis.get(id.toString(), (err, ok) => {
-    if (err || !ok) return res.status(404).send("");
-    return res.status(204).send("");
-  });
+  if (await CheckUser(id)) return res.status(204).send("");
+  res.status(404).send("");
 }
