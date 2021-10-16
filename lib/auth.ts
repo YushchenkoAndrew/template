20c693828c996eb06d740bba3019532c9e902780
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-10-17 02:17:16 +0300

    Move api auth to separet file

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,8 @@
+import { apiHost } from "../config";
+import redis from "../config/redis";
+import { ApiError, ApiTokens } from "../types/api";
+import md5 from "./md5";
+
 export function PassValidate(pass: string, pass2: string) {
   let equal = true;
   let max = pass.length > pass2.length ? pass.length : pass2.length;
@@ -9,4 +14,72 @@ export function PassValidate(pass: string, pass2: string) {
       pass2.charAt(i) == pass.charAt(i);
   }
   return equal;
+}
+
+export function UpdateTokens(data: ApiTokens) {
+  redis.set("API:Access", data.access_token);
+  redis.set("API:Refresh", data.refresh_token);
+
+  redis.expire("API:Access", 15 * 60);
+  redis.expire("API:Refresh", 7 * 24 * 60 * 60);
+}
+
+export function ApiAuth() {
+  return new Promise((resolve, reject) => {
+    redis.get("API:Access", (err, reply) => {
+      if (!err && reply) return resolve(reply);
+
+      // If Access token expired, then refresh token
+      redis.get("API:Refresh", (err, reply) => {
+        if (!err && reply) {
+          return fetch(`http://${apiHost}/api/refresh`, {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({
+              refresh_token: reply,
+            }),
+          })
+            .then((res) => res.json())
+            .then((data: ApiTokens | ApiError) => {
+              if (data.status == "ERR")
+                return reject("Incorrect refresh token value");
+
+              UpdateTokens(data as ApiTokens);
+              resolve((data as ApiTokens).access_token);
+            })
+            .catch((err) => reject(err));
+        }
+
+        // If Refresh token expired, then relogin
+        let salt = Math.round(Math.random() * 10000).toString();
+        fetch(`http://${apiHost}/api/login`, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            user: process.env.API_USER ?? "",
+            pass:
+              salt +
+              "$" +
+              md5(
+                (process.env.API_PEPPER ?? "") +
+                  salt +
+                  (process.env.API_PASS ?? "")
+              ),
+          }),
+        })
+          .then((res) => res.json())
+          .then((data: ApiTokens | ApiError) => {
+            if (data.status == "ERR") return reject("Incorrect user or pass");
+
+            UpdateTokens(data as ApiTokens);
+            resolve((data as ApiTokens).access_token);
+          })
+          .catch((err) => reject(err));
+      });
+    });
+  });
 }
--- pages/api/cache/upload.ts
+++ pages/api/cache/upload.ts
@@ -1,23 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import redis from "../../../config/redis";
-import md5 from "../../../lib/md5";
-import { ApiTokens, ApiError } from "../../../types/api";
-import { apiHost, botHost } from "../../../config";
+import { apiHost } from "../../../config";
 import { formatDate } from "../../info";
-import { PassValidate } from "../../../lib/auth";
+import { ApiAuth, PassValidate } from "../../../lib/auth";
 import { getValue } from "../../../lib/mutex";
-import { LogMessage } from "../../../types/bot";
 import { sendLogs } from "../../../lib/bot";
-
-type QueryParams = { id: string };
-
-function UpdateTokens(data: ApiTokens) {
-  redis.set("API:Access", data.access_token);
-  redis.set("API:Refresh", data.refresh_token);
-
-  redis.expire("API:Access", 15 * 60);
-  redis.expire("API:Refresh", 7 * 24 * 60 * 60);
-}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
@@ -36,63 +23,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   }
 
   const now = formatDate(new Date());
-  new Promise((resolve, reject) => {
-    redis.get("API:Access", (err, reply) => {
-      if (!err && reply) return resolve(reply);
-
-      // If Access token expired, then refresh token
-      redis.get("API:Refresh", (err, reply) => {
-        if (!err && reply) {
-          return fetch(`http://${apiHost}/api/refresh`, {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify({
-              refresh_token: reply,
-            }),
-          })
-            .then((res) => res.json())
-            .then((data: ApiTokens | ApiError) => {
-              if (data.stat == "ERR")
-                return reject("Incorrect refresh token value");
-
-              UpdateTokens(data as ApiTokens);
-              resolve((data as ApiTokens).access_token);
-            })
-            .catch((err) => reject(err));
-        }
-
-        // If Refresh token expired, then relogin
-        let salt = Math.round(Math.random() * 10000).toString();
-        fetch(`http://${apiHost}/api/login`, {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            user: process.env.API_USER ?? "",
-            pass:
-              salt +
-              "$" +
-              md5(
-                (process.env.API_PEPPER ?? "") +
-                  salt +
-                  (process.env.API_PASS ?? "")
-              ),
-          }),
-        })
-          .then((res) => res.json())
-          .then((data: ApiTokens | ApiError) => {
-            if (data.stat == "ERR") return reject("Incorrect user or pass");
-
-            UpdateTokens(data as ApiTokens);
-            resolve((data as ApiTokens).access_token);
-          })
-          .catch((err) => reject(err));
-      });
-    });
-  })
+  ApiAuth()
     .then((access) => {
       redis.hgetall("Info:Now", (err, reply) => {
         if (err || !reply) return;
