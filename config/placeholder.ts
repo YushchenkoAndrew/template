671e54c6a93b274d13d0ca886d3e8c0f2eec58bd
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-10-18 23:28:42 +0300

    Move placeholder to config/

---
+++ config/placeholder.ts
@@ -0,0 +1,16 @@
+import { basePath } from ".";
+
+export const ProjectInfo = {
+  name: "CodeRain",
+  title: "Code Rain",
+  flag: "js",
+  img: {
+    name: "CodeRain.webp",
+    type: "webp",
+    role: "thumbnail",
+    url: `${basePath}/img/CodeRain.webp`,
+  },
+  desc: "Take the blue pill and the site will close, or take the red pill and I show how deep the rabbit hole goes",
+  note: "Creating a 'Code Rain' effect from Matrix. As funny joke you can put any text to display at the end.",
+  link: "github.com/YushchenkoAndrew/template/tree/master/JS/CodeRain",
+};
