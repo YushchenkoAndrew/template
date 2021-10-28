41170f65546eb3b332303370b8aa71add14254ea
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-10-29 02:19:03 +0300

    Add close option for alert

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,23 +1,28 @@
 import { useRouter } from "next/dist/client/router";
 import React, { CSSProperties, useEffect, useState } from "react";
-import Image from "react-bootstrap/Image";
-import styles from "./Card.module.css";
 
 export interface AlertProps {
-  state: string;
+  onClose?: () => void;
+
+  state?: string;
   title?: string;
   note?: string;
 }
 
 export default function Alert(props: AlertProps) {
-  return (
-    <div
-      className={`col align-self-center text-center alert ${props.state} ${
-        props.title ? "" : "d-none"
-      }`}
-    >
-      <h4 className="alert-heading">{props.title}</h4>
-      <p>{props.note}</p>
+  return !props.title ? (
+    <></>
+  ) : (
+    <div className={`alert ${props.state} alert-dismissible fade show`}>
+      <div className="text-center">
+        <span>
+          <h4 className="alert-heading">{props.title}</h4>
+          <p>{props.note}</p>
+        </span>
+        <button className="close" onClick={() => props.onClose?.()}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
     </div>
   );
 }
