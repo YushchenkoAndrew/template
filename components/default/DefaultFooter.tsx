238a1290f952784eddf9a914593f63163faea707
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-11-15 17:41:19 +0200

    Change Default footer
    .

--- components/default/DefaultFooter.tsx
+++ components/default/DefaultFooter.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import FooterTop from "../Footer/Top";
-import FooterBottom from "../Footer/Bottom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedinIn,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
-import { basePath } from "../../config";
+import styles from "./Footer.module.css";
+import DefaultLinks from "./DefaultLinks";
 
 export interface DefaultFooterProps {
+  className?: string;
   name: string;
   children: React.ReactNode;
 }
@@ -17,74 +11,38 @@ export interface DefaultFooterProps {
 export default function DefaultFooter(props: DefaultFooterProps) {
   return (
     <div className="container">
-      <footer className="pt-4 my-md-5 pt-md-5 border-top py-2">
+      <footer
+        className={props.className ?? `pt-4 my-md-5 pt-md-5 border-top py-2`}
+      >
         <div className="row justify-content-center">
           <div className="col-11">
-            <FooterTop author="Andrew Y" name={props.name}>
-              {props.children}
-            </FooterTop>
-            <FooterBottom email="AndrewYushchenko@gmail.com">
-              <a
-                className="text-muted mx-2 mx-md-3"
-                href="https://github.com/YushchenkoAndrew"
-                onClick={() =>
-                  localStorage.getItem("id")
-                    ? fetch(
-                        `${basePath}/api/view/media?id=${localStorage.getItem(
-                          "id"
-                        )}`,
-                        { method: "PATCH" }
-                      )
-                        .then((res) => null)
-                        .catch((err) => null)
-                    : null
-                }
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faGithub} size="lg" />
-              </a>
-              <a
-                className="text-muted mx-2 mx-md-3"
-                href="https://twitter.com/AndrewY69942173"
-                onClick={() =>
-                  localStorage.getItem("id")
-                    ? fetch(
-                        `${basePath}/api/view/media?id=${localStorage.getItem(
-                          "id"
-                        )}`,
-                        { method: "PATCH" }
-                      )
-                        .then((res) => null)
-                        .catch((err) => null)
-                    : null
-                }
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faTwitter} size="lg" />
-              </a>
-              <a
-                className="text-muted mx-2 mx-md-3"
-                href="https://www.linkedin.com/in/andrew-yushchenko-7447771a2/"
-                onClick={() =>
-                  localStorage.getItem("id")
-                    ? fetch(
-                        `${basePath}/api/view/media?id=${localStorage.getItem(
-                          "id"
-                        )}`,
-                        { method: "PATCH" }
-                      )
-                        .then((res) => null)
-                        .catch((err) => null)
-                    : null
-                }
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
-              </a>
-            </FooterBottom>
+            <div className="row">
+              <div className="col col-md-5 col-sm-7 mx-sm-auto">
+                <div className="d-flex flex-column h-100">
+                  <h3
+                    className={`text-dark row justify-content-sm-center ${styles["author"]}`}
+                  >
+                    Andrew Y
+                  </h3>
+
+                  <div className="d-none d-sm-block d-md-block d-lg-block d-xl-block mt-auto">
+                    <div className="row justify-content-center">
+                      <DefaultLinks />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-9 col-md-4 col-sm-5">
+                <h6 className="mb-3 mb-lg-4 bold-text ">
+                  <b>{props.name}</b>
+                </h6>
+                {props.children}
+              </div>
+            </div>
+
+            <div className="d-sm-none row justify-content-center">
+              <DefaultLinks />
+            </div>
           </div>
         </div>
       </footer>
