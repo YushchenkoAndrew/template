fa210719e2f10cbcfb8258711f7df1be95944af5
Andrew Y <Andrew10122000@gmail.com>
2023-05-30 22:25:29 +0300

    Add redirect to /projects

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = require("./routes");
 const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8000;
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+app.get("/", (req, res) => res.redirect("/projects"));
 
 // Simple request for check it Server is alive
 app.get("/ping", (req, res) => res.send("pong"));
@@ -18,10 +19,14 @@ app.use("/projects", router);
 app.use("/projects", express.static("JS"));
 
 // Catch 404 and forward to error handler
-app.use((req, res, next) => errorHandler(404, `Not Found '${req.url}'`, req, res));
+app.use((req, res, next) =>
+  errorHandler(404, `Not Found '${req.url}'`, req, res)
+);
 
 // Error handler
-app.use((err, req, res, next) => errorHandler(err.status, err.message.split("\n")[0], req, res));
+app.use((err, req, res, next) =>
+  errorHandler(err.status, err.message.split("\n")[0], req, res)
+);
 
 process.on("SIGINT", function () {
   logInfo("Server Terminated");
